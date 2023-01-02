0c924a9fe601934da4cac58df5e6405005cbb30f
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-01-02 16:19:49 +0000

    still fixing the animation with yield
    the scene still doesn't change

--- src/behind_the_scenes/scene_manager.gd
+++ src/behind_the_scenes/scene_manager.gd
@@ -6,9 +6,12 @@ extends Node
 onready var animation = $CanvasLayer/AnimationPlayer
 var scene : String
 
-func change_scene(new_scene, anim):
-	scene = new_scene
+func change_scene(anim, new_scene, anim2):
 	animation.play(anim)
+	yield(get_tree().create_timer(1.0), "timeout")
+	scene = new_scene
+	yield(get_tree().create_timer(1.0), "timeout")
+	animation.play_backwards(anim2)
 
 func _new_scene():
 		get_tree().change_scene(scene)
