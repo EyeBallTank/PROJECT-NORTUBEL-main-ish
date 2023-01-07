880108869b9831c122d23560d87a1b885a21c9eb
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-01-07 00:22:38 +0000

    menu stuff
    still working on it

--- screens/MainMenu.gd
+++ screens/MainMenu.gd
@@ -1,6 +1,7 @@
 extends Control
 
-
+onready var menu = $MenuMain
+onready var options = $OptionsMenu
 
 func _on_StartButton_pressed():
 	SceneManager.change_scene("level_transition_effect", "res://src/levels/LevelTemplate.tscn")
@@ -8,3 +9,30 @@ func _on_StartButton_pressed():
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+
+
+func show_and_hide(first, second):
+	first.show()
+	second.hide()
+
+func _on_OptionsButton_pressed():
+	show_and_hide(options, menu)
+
+
+func _on_BackFromOptions_pressed():
+	show_and_hide(menu, options)
+
+
+func _on_Borderless_toggled(button_pressed):
+	OS.window_borderless = button_pressed
+
+
+func _on_Music_value_changed(value):
+	volume(1,value)
+
+func _on_Sound_value_changed(value):
+	volume(2,value)
+
+func volume(bus_index, value):
+	AudioServer.set_bus_volume_db(bus_index, linear2db(value)-30)
