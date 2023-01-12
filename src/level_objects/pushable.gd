63e3e46890c8912d620e74981f9e3ab6cd6b185d
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-01-12 18:44:32 +0000

    halfway there with the pushable object
    still WIP

---
+++ src/level_objects/pushable.gd
@@ -0,0 +1,10 @@
+extends RigidBody2D
+class_name Pushable
+
+
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+	angular_velocity = 0
+	rotation_degrees = 0
+
+func _ready():
+	pass
