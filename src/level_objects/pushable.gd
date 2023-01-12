cc88f9f8e4f4fccea303ea169c236db37d0d653a
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-01-12 20:23:57 +0000

    deleted commented code
    i keep frankensteining stuff

--- src/level_objects/pushable.gd
+++ src/level_objects/pushable.gd
@@ -1,7 +1,6 @@
 extends RigidBody2D
 class_name Pushable
 
-
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	angular_velocity = 0
 	rotation_degrees = 0
--- src/play_actors/actor.gd
+++ src/play_actors/actor.gd
@@ -4,17 +4,9 @@ class_name actor
 # This code is copied from the platformer tutorial by GDQuest
 
 const FLOOR_NORMAL: = Vector2.UP
-#const PUSH: = 5
+
 
 export var speed: = Vector2(300.0, 1000.0)
 export var gravity: = 3000.0
 
 var velocity: = Vector2.ZERO
-#var inertia = move_and_slide(velocity, Vector2.UP, false, 4, PI/4, false)
-
-#func _physics_process(delta):
-
-#	for index in get_slide_count():
-#		var collision = get_slide_collision(index)
-#		if collision.collider is Pushable:
-#			collision.collider.apply_central_impulse(-collision.normal * PUSH)
