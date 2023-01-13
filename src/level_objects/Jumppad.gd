a1008a18598b38b4720613519456c213837f7800
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-01-13 16:59:06 +0000

    got a jumppad and it works kinda
    not bad
    credit to hellofellowjell-0186 on YT for the video i used as reference

---
+++ src/level_objects/Jumppad.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		body.velocity.y = -2000
+		body.move_and_slide(body.velocity)
+#CODE COPIED FROM hellofellowjell-0186 ON YOUTUBE
