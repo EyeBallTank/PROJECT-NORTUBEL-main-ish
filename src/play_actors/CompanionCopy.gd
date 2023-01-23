491f06ad2ddf7d0ce7dc442d2f1cee66690dba65
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-01-23 17:08:39 +0000

    rearranged the scripts of copied actors
    no they no longer need the actor script but there's more changes i wanna do
    the physics of the copied player do feel different somehow

---
+++ src/play_actors/CompanionCopy.gd
@@ -0,0 +1,97 @@
+extends KinematicBody2D
+
+
+const FLOOR_NORMAL: = Vector2.UP
+
+
+export var speed: = Vector2(300.0, 1000.0)
+export var gravity: = 3000.0
+
+var velocity: = Vector2.ZERO
+
+
+
+
+
+#I took Gonkee's Jared AI code and tried to change it to my preferences.
+#And the state machine was based on a suggestion by TebyTheCat on Newgrounds.
+
+enum {
+	FOLLOWME,
+	STANDSTILL
+#	CRAWL,
+#	CLIMBIDLE,
+#	CLIMBMOVE
+#	PUSH
+}
+
+var state = STANDSTILL
+
+onready var Player = get_parent().get_node("Player")
+
+var vel = Vector2(0, 0)
+
+var grav = 1800
+var max_grav = 3000
+
+var react_time = 400
+var dir = 0
+var next_dir = 0
+var next_dir_time = 0
+
+var next_jump_time = -1
+
+var target_player_distance = 90
+
+func _ready():
+	set_process(true)
+
+
+func set_dir(target_dir):
+	if next_dir != target_dir:
+		next_dir = target_dir
+		next_dir_time = OS.get_ticks_msec() + react_time
+
+func _process(delta):
+	match state:
+		FOLLOWME:
+			if Player.position.x < position.x - target_player_distance:
+				set_dir(-1)
+			elif Player.position.x > position.x + target_player_distance:
+				set_dir(1)
+			else:
+				set_dir(0)
+
+			if OS.get_ticks_msec() > next_dir_time:
+				dir = next_dir
+	
+			if OS.get_ticks_msec() > next_jump_time and next_jump_time != -1 and is_on_floor():
+				if Player.position.y < position.y - 64:
+					vel.y = -800
+				next_jump_time = -1
+	
+			if Player.position.y < position.y - 64 and next_jump_time == -1:
+				next_jump_time = OS.get_ticks_msec() + react_time
+		STANDSTILL:
+			set_dir(0)
+			vel.x = 0
+			target_player_distance = 0
+			next_dir = 0
+			next_dir_time = 0
+			dir = 0
+
+	vel.y += grav * delta;
+	if vel.y > max_grav:
+		vel.y = max_grav
+
+	if is_on_floor() and vel.y > 0:
+		vel.y = 0
+		
+	vel.x = dir * 500
+
+	vel = move_and_slide(vel, Vector2(0, -1))
+		
+	if Input.is_action_pressed("followme"):
+		state = FOLLOWME
+	elif Input.is_action_pressed("standstill"):
+		state = STANDSTILL
---
+++ src/play_actors/PlayerCopy.gd
@@ -0,0 +1,55 @@
+extends KinematicBody2D
+
+const FLOOR_NORMAL: = Vector2.UP
+
+export var speed: = Vector2(300.0, 1000.0)
+export var gravity: = 3000.0
+
+var velocity: = Vector2.ZERO
+
+
+# EyeBall here, as you can see i copied code from the Cubio demo
+# My IRL schedule is a bit of a mess, so don't expect professionalism that much
+# Credit to the Godot team and anyone for making resources that i keep Frankensteining into my projects
+
+#The following comments are not my own, if that isn't clear enough
+
+#THESE ARE PREPERATIONS FOR FUTURE PLAYER SATES
+#enum {
+#	MAINSTATE,
+#	CLIMB,
+#	PUSH,
+#	SWIM,
+#	SWING,
+#	KICK,
+#	KNIFE
+#}
+
+#var state = MAINSTATE
+
+const WALK_FORCE = 1600
+const WALK_MAX_SPEED = 700
+const STOP_FORCE = 900
+const JUMP_SPEED = 1500
+
+func _physics_process(delta):
+	# Horizontal movement code. First, get the player's input.
+	var walk = WALK_FORCE * (Input.get_action_strength("right") - Input.get_action_strength("left"))
+	# Slow down the player if they're not trying to move.
+	if abs(walk) < WALK_FORCE * 0.1:
+		# The velocity, slowed down a bit, and then reassigned.
+		velocity.x = move_toward(velocity.x, 0, STOP_FORCE * delta)
+	else:
+		velocity.x += walk * delta
+	# Clamp to the maximum horizontal movement speed.
+	velocity.x = clamp(velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED)
+
+	# Vertical movement code. Apply gravity.
+	velocity.y += gravity * delta
+
+	# Move based on the velocity and snap to the ground.
+	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+
+	# Check for jumping. is_on_floor() must be called after movement code.
+	if is_on_floor() and Input.is_action_just_pressed("jumpup"):
+		velocity.y = -JUMP_SPEED
