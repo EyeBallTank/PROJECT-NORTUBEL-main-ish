2579d184e507b5c1a01edc1db29499fbe98113eb
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-01-28 02:11:44 +0000

    bringing it back

--- src/level_objects/PushableCopy.gd
+++ src/level_objects/PushableCopy.gd
@@ -5,12 +5,12 @@ const FLOOR_NORMAL: = Vector2.UP
 export var speed: = Vector2(300.0, 1000.0)
 export var gravity: = 3000.0
 
-#enum {
-#	PUSHED,
-#	FROZEN
-#}
-#
-#var state = FROZEN
+enum {
+	PUSHED,
+	FROZEN
+}
+
+var state = FROZEN
 
 var velocity: = Vector2.ZERO
 
@@ -22,20 +22,20 @@ var JUMP_SPEED = 1500
 onready var pushableside = $leftandrightside
 
 func _physics_process(delta):
-##	match state:
-##		FROZEN:
+	match state:
+		FROZEN:
 			velocity.x = move_toward(velocity.x, 0, STOP_FORCE * delta)
 
 			velocity.y += gravity * delta
 
 			velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
-#
-##		PUSHED:
-#			WALK_MAX_SPEED = 150
-#			if Input.get_action_strength("right"):
-#				velocity.x = WALK_MAX_SPEED
-#			elif Input.get_action_strength("left"):
-#				velocity.x = -WALK_MAX_SPEED
-#			else:
-#				velocity.x = move_toward(velocity.x, 0, STOP_FORCE * delta)
-#				state = FROZEN
+
+		PUSHED:
+			WALK_MAX_SPEED = 150
+			if Input.get_action_strength("right"):
+				velocity.x = WALK_MAX_SPEED
+			elif Input.get_action_strength("left"):
+				velocity.x = -WALK_MAX_SPEED
+			else:
+				velocity.x = move_toward(velocity.x, 0, STOP_FORCE * delta)
+				state = FROZEN
