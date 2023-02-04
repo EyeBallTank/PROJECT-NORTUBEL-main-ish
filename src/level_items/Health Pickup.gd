20a2b900aa3f646670e4c642a5b755f906183dcb
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-02-04 13:50:31 +0000

    health pickup cannot be obtain if health is full
    BUT somehow, the Companion cannot obtain it even if health is under the main value

--- src/level_items/Health Pickup.gd
+++ src/level_items/Health Pickup.gd
@@ -9,5 +9,18 @@ func _ready():
 
 func _on_Health_Pickup_body_entered(body):
 	if body.name == "Player":
-		body.health += 10
-		queue_free()
+		if body.health < 100:
+			body.health += 10
+			queue_free()
+		else:
+			pass
+
+	if body.name == "Companion":
+		if body.health < 50:
+			body.health += 10
+			queue_free()
+		else:
+			pass
+
+#Wanted to make sure one character touching the Health Pickup heals BOTH
+#But it seems the Companion cannot obtain the Health Pickup to heal
