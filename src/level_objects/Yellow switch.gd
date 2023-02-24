d14a09dddcb0dc0d69ad55154e58e31674b74903
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-02-24 22:47:41 +0000

    trying to create the yellow switch
    i know i should use a proper code for the animation ending so i don't use the yield timer but that's for later

--- src/level_objects/Yellow switch.gd
+++ src/level_objects/Yellow switch.gd
@@ -2,14 +2,17 @@ extends Area2D
 
 var is_pressed = false
 
+onready var animationplayer = $AnimationPlayer
 
 func _ready():
 	pass
-
 
 func _on_YellowSwitch_body_entered(body):
 	if body.name == "Player":
 		is_pressed = true
 		print("YELLOW IS PRESSED")
+		animationplayer.play("PRESSED")
 		yield(get_tree().create_timer(3), "timeout")
+		is_pressed = false
 		print("YELLOW IS UNPRESSED AGAIN")
+		animationplayer.play("UNPRESSED")
