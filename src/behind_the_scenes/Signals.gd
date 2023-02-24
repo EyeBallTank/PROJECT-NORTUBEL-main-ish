690b08ab06e9d333eca6b94f0663de71dbd395d7
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-02-24 17:09:21 +0000

    just because

--- src/behind_the_scenes/Signals.gd
+++ src/behind_the_scenes/Signals.gd
@@ -1,7 +1,12 @@
 extends Node
 
-signal blue_switch_pressed(blueswitchresponse)
-signal blue_plat_horiz_active(blueactivation)
+signal blue_switch_pressed
+signal blue_plat_horiz_active
 
 func _ready():
 	pass
+
+
+#signal blue_switch_pressed(blueswitchresponse)
+#signal blue_plat_horiz_active(blueactivation)
+#Just in case
