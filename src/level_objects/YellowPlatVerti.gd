c28c3a541b4c137c214954a0a12511472ef22c28
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-02-25 22:52:30 +0000

    not yet

--- src/level_objects/YellowPlatVerti.gd
+++ src/level_objects/YellowPlatVerti.gd
@@ -4,3 +4,28 @@ extends Node2D
 
 func _ready():
 	pass 
+
+#onready var animationplayer = $AnimationPlayer
+
+#enum {
+#	FROZENPLAT,
+#	ACTIVEPLAT
+#}
+
+#var state = FROZENPLAT
+
+#func _ready():
+#	Signals.connect("yellow_switch_pressed", self, "_on_YellowSwitch_pressed")
+#
+#func _process(delta):
+#	match state:
+#		FROZENPLAT:
+#			pass
+#		ACTIVEPLAT:
+#			animationplayer.play("HorizontalPlat")
+#
+#func _on_YellowSwitch_pressed():
+#	print("YELPLAT IS ACTIVE")
+#	Signals.emit_signal("yellow_plat_horiz_active")
+#	state = ACTIVEPLAT
+#
