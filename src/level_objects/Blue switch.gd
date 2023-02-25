95a66cc7c20f550c51af196a94841fdb0e7d4842
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-02-25 22:22:57 +0000

    Blue switch change
    Now the collisionshape turns off forever, so the switch can't be pressed anymore in the level

--- src/level_objects/Blue switch.gd
+++ src/level_objects/Blue switch.gd
@@ -3,6 +3,7 @@ extends Area2D
 var is_pressed = false
 
 onready var BluePlatHoriz = get_parent().get_node("BluePlatHoriz")
+onready var animationplayer = $AnimationPlayer
 
 func _ready():
 	pass
@@ -11,6 +12,7 @@ func _on_Blue_switch_body_entered(body):
 	if body.name == "Player":
 #		if Input.get_action_strength("attack"):
 		is_pressed = true
+		animationplayer.play("PRESSED")
 		Signals.emit_signal("blue_switch_pressed")
 		print("BLUE IS PRESSED")
 	else:
