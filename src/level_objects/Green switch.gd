4ac599b4ea8227859f7e319b67cfca88bf2eb769
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-02-26 15:20:47 +0000

    getting there

--- src/level_objects/Green switch.gd
+++ src/level_objects/Green switch.gd
@@ -1,6 +1,24 @@
 extends Area2D
 
-#LATER
+var is_pressed = false
+
+enum {
+	PRESSONE,
+	PRESSTWO
+}
+
+var state = PRESSONE
 
 func _ready():
 	pass
+
+func _process(delta):
+	match state:
+		PRESSONE:
+			pass
+		PRESSTWO:
+			pass
+
+func _on_GreenSwitch_body_entered(body):
+	if body.name == "Player":
+		is_pressed = true
