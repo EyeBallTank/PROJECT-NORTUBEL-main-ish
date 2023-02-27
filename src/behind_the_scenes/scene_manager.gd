f2d057527ec383cbe425e4b0ca3fe020de36bce2
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-02-27 23:31:28 +0000

    scene_manager now pauses the game and unpauses it when changing scene

--- src/behind_the_scenes/scene_manager.gd
+++ src/behind_the_scenes/scene_manager.gd
@@ -9,6 +9,13 @@ var scene : String
 func change_scene(anim, new_scene):
 	scene = new_scene
 	animation.play(anim)
+	
 
 func _new_scene():
 		get_tree().change_scene(scene)
+
+func pause_the_game():
+	get_tree().paused = true
+
+func unpause_the_game():
+	get_tree().paused = false
