e17449388d9f96feda7e52090155641a9344e9c7
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-02-27 23:38:43 +0000

    gong can only be touched once
    no more repeated sound

--- src/level_objects/Gong checkpoint.gd
+++ src/level_objects/Gong checkpoint.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 onready var audioPlayer = $AudioStreamPlayer
+onready var animationplayer = $AnimationPlayer
 
 func _ready():
 	pass
@@ -9,3 +10,4 @@ func _ready():
 func _on_Gong_body_entered(body):
 	if body.is_in_group("protagonists"):
 		audioPlayer.play()
+		animationplayer.play("TOUCHED")
