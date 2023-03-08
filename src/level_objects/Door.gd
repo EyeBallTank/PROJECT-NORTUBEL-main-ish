f5728a5cc075ff20128d30dda47a66f8427097e6
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-03-08 19:35:30 +0000

    endlevel door code change

--- src/level_objects/Door.gd
+++ src/level_objects/Door.gd
@@ -2,14 +2,10 @@ extends Area2D
 
 #Should probably make it so endlevel doors and such require both player and companion to end level
 
-var player = false
-var companion = false
-
 func _ready():
 	pass 
 
 
 func _on_Door_body_entered(body):
-	player = body
-	player = true
-	SceneManager.change_scene("level_transition_effect", "res://src/levels/LevelTemplate2.tscn")
+	if body.is_in_group("protagonists"):
+		SceneManager.change_scene("level_transition_effect", "res://src/levels/LevelTemplate2.tscn")
