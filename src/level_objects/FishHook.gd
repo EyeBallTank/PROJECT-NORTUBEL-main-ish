0cdc29d365cddff05c3aa6217dd2305a279c501e
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-03-10 12:44:07 +0000

    somewhat a step

--- src/level_objects/FishHook.gd
+++ src/level_objects/FishHook.gd
@@ -1,12 +1,26 @@
 extends RigidBody2D
 class_name FishHook
 
+var vel = Vector2.ZERO
+var hasplayer = false
+
 func _ready():
 	pass
 
-func _process(delta):
-	if Input.is_action_pressed("right"):
-		apply_central_impulse(Vector2(1000, 0))
+func _physics_process(delta):
+	if hasplayer == true:
+		if Input.is_action_pressed("right"):
+			apply_central_impulse(Vector2(800, 0))
+			
+	if hasplayer == true:
+		if Input.is_action_pressed("left"):
+			apply_central_impulse(Vector2(-800, 0))
 
-	if Input.is_action_pressed("left"):
-		apply_central_impulse(Vector2(-1000, 0))
+
+func _on_DetectableRope_body_entered(body):
+	if body.name == "Player":
+		hasplayer = true
+
+func _on_DetectableRope_body_exited(body):
+	if body.name == "Player":
+		hasplayer = false
