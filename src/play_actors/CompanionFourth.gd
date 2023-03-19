ef8e8b95d22b8e6c0e087743bb8e35867021073d
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-03-19 14:58:24 +0000

    a bit acceptable

--- src/play_actors/CompanionFourth.gd
+++ src/play_actors/CompanionFourth.gd
@@ -4,12 +4,12 @@ var WALK_FORCE = 1600
 var WALK_MAX_SPEED = 70
 var PUSH_SPEED = 150
 var STOP_FORCE = 450
-var JUMP_SPEED = 1990
+var JUMP_SPEED = 1390
 
-export var gravity: = 30000.0
+export var gravity: = 1400.0
 
 var velocity: = Vector2.ZERO
-var direction = Vector2.ZERO
+var direction: = Vector2.ZERO
 onready var Player = get_parent().get_node("Player")
 
 func _ready():
@@ -25,7 +25,7 @@ func _physics_process(delta):
 	else:
 		velocity.x = 0
 		direction.x = 0
-	velocity.x = direction.x * 280
+	velocity.x = direction.x * 310
 	
 	
 	velocity.y += gravity * delta
