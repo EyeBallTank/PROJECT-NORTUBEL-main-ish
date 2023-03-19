457254d9063908826770b8739e37279a8982a1f6
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-03-19 14:15:23 +0000

    added a 4th one

---
+++ src/play_actors/CompanionFourth.gd
@@ -0,0 +1,38 @@
+extends KinematicBody2D
+
+var WALK_FORCE = 1600
+var WALK_MAX_SPEED = 70
+var PUSH_SPEED = 150
+var STOP_FORCE = 450
+var JUMP_SPEED = 1500
+
+export var gravity: = 30000.0
+
+var velocity: = Vector2.ZERO
+var direction = Vector2.ZERO
+onready var Player = get_parent().get_node("Player")
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	if Player.global_position.x < global_position.x - 10:
+		velocity.x = WALK_MAX_SPEED
+		direction.x = -1
+	elif Player.global_position.x > global_position.x + 10:
+		velocity.x = -WALK_MAX_SPEED
+		direction.x = 1
+	else:
+		velocity.x = 0
+		direction.x = 0
+	velocity = direction * 280
+	
+	
+	velocity.y += gravity * delta
+	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+	
+	if is_on_floor() and Input.is_action_just_pressed("jumpup"):
+		velocity.y = -JUMP_SPEED
+	if Input.is_action_just_released("jumpup"):
+		if velocity.y < 0:
+			velocity.y -= 50
