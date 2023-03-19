a703cb728a96689cc2f4af2f557e3bfe7675841c
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-03-19 17:01:04 +0000

    giving the purple one commands

--- src/play_actors/CompanionFourth.gd
+++ src/play_actors/CompanionFourth.gd
@@ -1,5 +1,30 @@
 extends KinematicBody2D
 
+enum {
+	FOLLOWME,
+	STANDSTILL,
+	CRAWLIDLE,
+	CRAWLFOLLOW,
+	CRAWLRUN,
+	CLIMBIDLE,
+	CLIMBMOVE,
+	CLIMBRUN,
+#	PUSHFOLLOW
+#	PUSHRUN
+#	DEATH
+	SLOWRUN,
+	SLOWFOLLOW,
+#	ICE
+#	ICEFOLLOW
+#	ICERUN
+	SWIMIDLE,
+	SWIMMING,
+	SWIMRUN,
+	RUNAWAY
+}
+
+var state = FOLLOWME
+
 var WALK_FORCE = 1600
 var WALK_MAX_SPEED = 70
 var PUSH_SPEED = 150
@@ -16,22 +41,51 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	if Player.global_position.x < global_position.x - 10:
-		velocity.x = WALK_MAX_SPEED
-		direction.x = -1
-	elif Player.global_position.x > global_position.x + 10:
-		velocity.x = -WALK_MAX_SPEED
-		direction.x = 1
-	else:
-		velocity.x = 0
-		direction.x = 0
-	velocity.x = direction.x * 550
+	match state:
+		FOLLOWME:
+			if Player.global_position.x < global_position.x - 10:
+				velocity.x = WALK_MAX_SPEED
+				direction.x = -1
+			elif Player.global_position.x > global_position.x + 10:
+				velocity.x = -WALK_MAX_SPEED
+				direction.x = 1
+			else:
+				velocity.x = 0
+				direction.x = 0
+			velocity.x = direction.x * 550
 	
 	
-	velocity.y += gravity * delta
-	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+			velocity.y += gravity * delta
+			velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
 	
-	if is_on_floor() and Player.global_position.y < global_position.y - 10:
-		velocity.y = -JUMP_SPEED
-		if velocity.y < 0:
-			velocity.y += 500
+			if is_on_floor() and Player.global_position.y < global_position.y - 10:
+				velocity.y = -JUMP_SPEED
+				if velocity.y < 0:
+					velocity.y += 500
+
+			if Input.is_action_pressed("runaway"):
+				state = RUNAWAY
+
+		RUNAWAY:
+			if Player.global_position.x < global_position.x - 10:
+				velocity.x = WALK_MAX_SPEED
+				direction.x = 1
+			elif Player.global_position.x > global_position.x + 10:
+				velocity.x = -WALK_MAX_SPEED
+				direction.x = -1
+			else:
+				velocity.x = 0
+				direction.x = 0
+			velocity.x = direction.x * 550
+	
+	
+			velocity.y += gravity * delta
+			velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+	
+			if is_on_floor() and Player.global_position.y < global_position.y - 10:
+				velocity.y = -JUMP_SPEED
+				if velocity.y < 0:
+					velocity.y += 500
+
+			if Input.is_action_pressed("followme"):
+				state = FOLLOWME
