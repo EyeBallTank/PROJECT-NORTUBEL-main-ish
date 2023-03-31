43e798374eab5a8b1796adf4a7947278a32feb54
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-03-31 12:44:43 +0100

    resized player's collisions during water
    just to keep sprite mirroring in mind

--- src/play_actors/PlayerCopy.gd
+++ src/play_actors/PlayerCopy.gd
@@ -422,14 +422,14 @@ func _physics_process(delta):
 				global_rotation = 0
 				state = MAINSTATE
 		SWIM:
-			$CollisionShape2D.shape.extents = Vector2(118.5, 58.5)
-			$CollisionShape2D.position = Vector2(8.5, -158.5)
-			hurtboxcollision.shape.extents = Vector2(118.5, 59)
-			hurtboxcollision.position = Vector2(8.5, -158)
-			$RopeCheck/CollisionShape2D.shape.extents = Vector2(29.375, 14.5)
-			$RopeCheck/CollisionShape2D.position = Vector2(2.125, 21.25)
-			$PortalCheck/CollisionShape2D.shape.extents = Vector2(118, 59)
-			$PortalCheck/CollisionShape2D.position = Vector2(9, -158)
+			$CollisionShape2D.shape.extents = Vector2(104.5, 50.5 )
+			$CollisionShape2D.position = Vector2(0.5, -161.5)
+			hurtboxcollision.shape.extents = Vector2(104.5, 50.5)
+			hurtboxcollision.position = Vector2(0.5, -161.5)
+			$RopeCheck/CollisionShape2D.shape.extents = Vector2(26.125, 12.625)
+			$RopeCheck/CollisionShape2D.position = Vector2(0.125, 20.375)
+			$PortalCheck/CollisionShape2D.shape.extents = Vector2(104.5, 50)
+			$PortalCheck/CollisionShape2D.position = Vector2(0.5, -162)
 			PUSH_SPEED = 350
 			if Input.get_action_strength("right"):
 				velocity.x = PUSH_SPEED
