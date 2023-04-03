c4ae811c78ca61a5483d0eefe9ee7d5a38f1ca52
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-04-04 00:15:26 +0100

    setting up a basic npc

--- src/npc_actors/Bonka NPC.gd
+++ src/npc_actors/Bonka NPC.gd
@@ -3,5 +3,12 @@ extends KinematicBody2D
 #NOT YET
 #MAY GIVE HER ACTUAL SPRITES AND STUFF SOON
 
+export var gravity: = 2960
+var velocity = Vector2.ZERO
+
 func _ready():
 	pass
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
