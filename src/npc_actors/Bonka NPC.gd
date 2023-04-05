eb2b6a43d535603be2bdcb23f18aef0cf1401fdd
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-04-05 19:13:34 +0100

    trying to figure something

--- src/npc_actors/Bonka NPC.gd
+++ src/npc_actors/Bonka NPC.gd
@@ -6,9 +6,20 @@ extends KinematicBody2D
 export var gravity: = 2960
 var velocity = Vector2.ZERO
 
+onready var dialogue = $NinePatchRect
+
 func _ready():
+	dialogue.visible = false
 	pass
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+
+func _on_TalktoArea_body_entered(body):
+	if body.name == "Player":
+		dialogue.visible = true
+
+func _on_TalktoArea_body_exited(body):
+	if body.name == "Player":
+		dialogue.visible = false
