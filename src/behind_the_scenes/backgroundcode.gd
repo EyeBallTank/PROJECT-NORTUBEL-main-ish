9ddc3879905c0d49e3056990ca3ee44deef3471a
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-04-15 23:30:43 +0100

    so far every level has a bg

--- src/behind_the_scenes/backgroundcode.gd
+++ src/behind_the_scenes/backgroundcode.gd
@@ -3,11 +3,6 @@ extends ParallaxLayer
 
 export var background = "res://assets/sprites/level_backgrounds/level bg test 2.png"
 onready var spritenode = $Sprite
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	spritenode.texture = load(background)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
