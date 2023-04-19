221ede802c6c4653180cdb227156d16a6c9cbdcf
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-04-19 14:38:58 +0100

    coin sound effect

--- src/level_items/Coin test.gd
+++ src/level_items/Coin test.gd
@@ -1,10 +1,16 @@
 extends Area2D
 
+onready var audioPlayer = $AudioStreamPlayer
+onready var animationplayer = $AnimationPlayer
 
 func _ready():
+	animationplayer.play("RESET")
 	pass
 
 func _on_Coin_body_entered(body):
 	if body.is_in_group("protagonists"):
 		Signals.emit_signal("coin_was_captured")
+		animationplayer.play("collected")
+		audioPlayer.play()
+		yield(get_tree().create_timer(0.8), "timeout")
 		queue_free()
