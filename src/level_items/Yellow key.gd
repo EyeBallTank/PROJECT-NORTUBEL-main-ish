184919327c5ee6f1dd25534fe58965d19fe6827e
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-04-19 11:00:19 +0100

    key pickup sound and animation

--- src/level_items/Yellow key.gd
+++ src/level_items/Yellow key.gd
@@ -1,10 +1,17 @@
 extends Area2D
 
+onready var audioPlayer = $AudioStreamPlayer
+onready var animationplayer = $AnimationPlayer
+
 func _ready():
+	animationplayer.play("RESET")
 	pass 
 
 func _on_Yellow_key_body_entered(body: PhysicsBody2D):
 	if body.name == "Player":
 		body.hasyellowkey = true
+		audioPlayer.play()
+		animationplayer.play("caughtkey")
+		yield(get_tree().create_timer(0.9), "timeout")
 		queue_free()
-
+		
