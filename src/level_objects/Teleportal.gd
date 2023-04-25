42508f2b620f9dea3ef4ad142cbf026cc4f75f19
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-04-25 23:59:12 +0100

    portal has sounds too
    i had this in a while but now i used it

--- src/level_objects/Teleportal.gd
+++ src/level_objects/Teleportal.gd
@@ -3,9 +3,11 @@ extends Area2D
 export(int) var id = 0
 
 var lockPortal = false
+onready var audioplayer = $AudioStreamPlayer
 
 func LockedPortal():
 	lockPortal = true
+	audioplayer.play()
 	yield(get_tree().create_timer(0.5), "timeout")
 	lockPortal = false
 
