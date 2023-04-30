d67212bd372db015dc84e3235594e4c764056dc9
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-04-30 21:20:49 +0100

    a bit more

--- src/level_objects/Soccel Goal.gd
+++ src/level_objects/Soccel Goal.gd
@@ -1,12 +1,18 @@
 extends StaticBody2D
 
+onready var animationplayer = $AnimationPlayer
 
 func _ready():
+	$AnimationPlayer.play("RESET")
 	pass
 
 
 func _on_BallDetector_area_entered(area):
 	if area.name == "GoalDetector":
 		$AudioStreamPlayer.play()
-		yield(get_tree().create_timer(1), "timeout")
-		queue_free()
+		animationplayer.play("ballreceived")
+#		yield(get_tree().create_timer(1), "timeout")
+#		queue_free()
+
+func disappear():
+	queue_free()
--- src/level_objects/Soccer Ball.gd
+++ src/level_objects/Soccer Ball.gd
@@ -7,16 +7,20 @@ onready var audioplayer = $AudioStreamPlayer
 var bounce_factor = 150
 
 func _ready():
+	animationplayer.play("RESET")
 	ball_disappear()
 
 func ball_disappear():
-	yield(get_tree().create_timer(2), "timeout")
+#	yield(get_tree().create_timer(2), "timeout")
 	animationplayer.play("Ballvanish")
-	yield(get_node("AnimationPlayer"), "animation_finished")
-	queue_free()
+#	yield(get_node("AnimationPlayer"), "animation_finished")
+#	queue_free()
 
 #audioplayer.play()
 
 func _on_SoccerBall_body_entered(_body):
 	if abs(self.linear_velocity.x)>bounce_factor or abs(self.linear_velocity.y)>bounce_factor:
 		audioplayer.play()
+
+func disappear():
+	queue_free()
