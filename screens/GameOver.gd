eb5e72962d527373b24b6b368d174f5e8d86ae49
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-05-04 12:07:35 +0100

    wait

--- screens/GameOver.gd
+++ screens/GameOver.gd
@@ -1,19 +1,10 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	Signals.lives = 3
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	Signals.score = 0
+#This one seems to work
 
 
 
