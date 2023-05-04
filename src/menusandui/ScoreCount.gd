5927cbe2cb0f2926f37dc313593a36232612b185
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-05-04 11:59:35 +0100

    still not much

--- src/menusandui/ScoreCount.gd
+++ src/menusandui/ScoreCount.gd
@@ -8,14 +8,21 @@ func _ready():
 	Signals.connect("coin_was_captured", self, "_on_CoinCaptured")
 
 func _on_CoinCaptured():
-	ezcudo_score += 1
+	Signals.score += 1
+	ezcudo_score = Signals.score
 	ScoreLabel.text = "Ezcudos: " + str(ezcudo_score)
 	Signals.emit_signal("score_was_updated")
-
-func _physics_process(_delta):
 	if ezcudo_score == 3:
-		ezcudo_score = 0
+		Signals.score = 0
+		ezcudo_score = Signals.score
 		ScoreLabel.text = "Ezcudos: " + str(ezcudo_score)
 		Signals.emit_signal("new_life")
+#Still returns to 0 in the next level.
+
+#func _physics_process(_delta):
+#	if ezcudo_score == 3:
+#		Signals.score = 0
+#		ScoreLabel.text = "Ezcudos: " + str(ezcudo_score)
+#		Signals.emit_signal("new_life")
 #		Signals.add_life()
 #		Signals.lives += 1
