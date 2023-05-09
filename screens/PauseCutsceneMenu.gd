1724efab270e5eee1ca14a0a93df98bfee3cbe5e
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-05-09 15:20:01 +0100

    so far so good

--- screens/PauseCutsceneMenu.gd
+++ screens/PauseCutsceneMenu.gd
@@ -3,5 +3,24 @@ extends Control
 #A pause menu dedicated to cutscenes
 #Ideally, it pauses a cutscene and can skip it
 
+export var nextlevel = "res://src/levels/LevelTemplate.tscn"
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		toggle()
+
+func toggle():
+	visible = !visible
+	get_tree().paused = visible
+
 func _ready():
-	pass
+	hide()
+
+
+func _on_UnpauseButton_pressed():
+	hide()
+	get_tree().paused = false
+
+
+func _on_SkipCutscene_pressed():
+	SceneManager.change_scene("level_transition_effect", nextlevel)
