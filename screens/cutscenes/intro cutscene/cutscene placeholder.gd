7bf48463ae9fd2a3c28794e1e13a51800976be5c
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-05-09 17:16:03 +0100

    more work on cutscene

--- screens/cutscenes/intro cutscene/cutscene placeholder.gd
+++ screens/cutscenes/intro cutscene/cutscene placeholder.gd
@@ -1,6 +1,7 @@
 extends Node
 
-onready var nextscene = "res://src/levels/LevelTemplate.tscn"
+onready var animationplayer = $AnimationPlayer
+export var nextscene = "res://src/levels/LevelTemplate.tscn"
 
 #maybe i should work on this later because i need to consider:
 #* HOW MODULAR WILL THESE TYPE OF SCENE BE IN OTHER INSTANCES/USES
@@ -8,6 +9,12 @@ onready var nextscene = "res://src/levels/LevelTemplate.tscn"
 #* HOW LONG CERTAIN IMAGES STAY, SINCE THERE WILL ALWAYS BE MUCH MORE TEXT TO CHANGE
 #* AND PROBABLY MORE
 
+func _ready():
+	animationplayer.play("intro cutscene")
+
 func _process(_delta):
 	if Input.is_action_just_pressed("kickball"):
 		SceneManager.change_scene("level_transition_effect", nextscene)
+
+func cutscene_over():
+	SceneManager.change_scene("level_transition_effect", nextscene)
