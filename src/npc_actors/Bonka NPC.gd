a6ac35e159a06db8920ca025aa463c4e41029dc7
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-05-22 00:28:11 +0100

    random stuff related to bonka

--- src/npc_actors/Bonka NPC.gd
+++ src/npc_actors/Bonka NPC.gd
@@ -7,8 +7,11 @@ export var gravity: = 2960
 var velocity = Vector2.ZERO
 
 onready var dialogue = $CanvasLayer
+onready var maintext = $CanvasLayer/NinePatchRect/Dialogue
+export(String) var customtext = "BONKA_TALK_1"
 
 func _ready():
+	maintext.text = str (customtext)
 	dialogue.visible = false
 	pass
 
