71ec589b343c3c8f6e08f377b2dc59f55848f869
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-06-08 23:31:35 +0100

    just in case

--- src/levels/LevelTemplate.gd
+++ src/levels/LevelTemplate.gd
@@ -1,12 +1,1 @@
-extends Node2D
-#class_name main_level_script
-
-func _tree_entered():  
-	#Send all new nodes to register_node()
-	get_tree().connect("node_added", self, "register_node") 
-
-
-func register_node(node:Node):
-	#Here is where you can add more "if" statements to filter which nodes are affected  
-	if node.owner == null:
-		node.owner = get_tree().current_scene
+extends main_level_script
