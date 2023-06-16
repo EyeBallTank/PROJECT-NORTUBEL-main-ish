160bc818375438229ba3fadcbee88819225ac4e5
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-06-16 11:34:06 +0100

    added more platforms

--- src/level_objects/WaterWheel.gd
+++ src/level_objects/WaterWheel.gd
@@ -1,10 +1,17 @@
 extends Node2D
 #onready var mainwheel = $KinematicBody2D
-onready var woodenplat = $KinematicBody2D/Woodplat1
+onready var woodenplat1 = $KinematicBody2D/Woodplat1
+onready var woodenplat2 = $KinematicBody2D/Woodplat2
+onready var woodenplat3 = $KinematicBody2D/Woodplat3
+onready var woodenplat4 = $KinematicBody2D/Woodplat4
+
 onready var animationplayer = $AnimationPlayer
 
 func _ready():
 	animationplayer.play("rotate")
 
 func _physics_process(delta):
-	woodenplat.global_rotation = 0
+	woodenplat1.global_rotation_degrees = 0
+	woodenplat2.global_rotation_degrees = 0
+	woodenplat3.global_rotation_degrees = 0
+	woodenplat4.global_rotation_degrees = 0
