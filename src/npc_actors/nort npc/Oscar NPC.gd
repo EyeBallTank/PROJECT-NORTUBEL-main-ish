d4107810535aaa2189317b1ac9984d9c2a05a15c
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-06-18 19:33:55 +0100

    so far, so good

---
+++ src/npc_actors/nort npc/Oscar NPC.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+#When not playable, protagonists can be NPC's
+export var gravity: = 2960
+var velocity = Vector2.ZERO
+var honk = false
+
+onready var dialogue = $CanvasLayer
+onready var maintext = $CanvasLayer/NinePatchRect/Dialogue
+export(String) var customtext = "OSCAR_TALK_1"
+export(String) var customtext3 = "OSCAR_HUG"
+export(String) var customtext4 = "OSCAR_HURT"
+
+func _ready():
+	maintext.text = str (customtext)
+	dialogue.visible = false
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+	if honk == true:
+		if Input.is_action_just_pressed("followme"):
+			maintext.text = str (customtext3)
+		if Input.is_action_just_pressed("attack"):
+			maintext.text = str (customtext4)
+
+func _on_TalktoArea_body_entered(body):
+	if body.name == "Player":
+		dialogue.visible = true
+		honk = true
+
+func _on_TalktoArea_body_exited(body):
+	if body.name == "Player":
+		dialogue.visible = false
+		honk = false
+		maintext.text = str (customtext)
