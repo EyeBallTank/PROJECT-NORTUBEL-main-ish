cd5e9df6b778b79018327bdc4c3368bbd38b3f35
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-06-19 21:28:00 +0100

    nvm didn't work the way i wanted

--- src/level prefabs/brink prefabs/Brink Big Straw Redwhite.gd
+++ src/level prefabs/brink prefabs/Brink Big Straw Redwhite.gd
@@ -2,15 +2,5 @@ extends StaticBody2D
 
 onready var sprite = $AnimatedSprite
 
-
 func _ready():
-	sprite.animation = "normal"
-
-func _on_GetInside_body_entered(body):
-	if body.is_in_group("protagonists"):
-		sprite.animation = "inside"
-
-
-func _on_GetInside_body_exited(body):
-	if body.is_in_group("protagonists"):
-		sprite.animation = "normal"
+	sprite.animation = "inside"
