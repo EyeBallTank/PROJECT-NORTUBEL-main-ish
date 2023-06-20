01b861193becbfd43a189acfb341b9d1a73a0a24
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-06-20 14:10:51 +0100

    trying to work out a van

---
+++ src/level prefabs/nort prefabs/Nort Market Van Redwhite.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+export var gravity: = 29600
+var direction = Vector2.RIGHT
+var velocity = Vector2.ZERO
+onready var sprite = $AnimatedSprite
+
+func _physics_process(delta):
+	velocity.x = 0
+	velocity.y += gravity * delta
+	move_and_slide(velocity, Vector2.UP)
+
+func _ready():
+	sprite.animation = "normal"
+
+
+func _on_Enter_body_entered(body):
+	if body.name == "Player":
+		sprite.animation = "open"
