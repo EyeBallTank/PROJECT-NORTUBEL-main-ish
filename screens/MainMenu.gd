b8aaef5b80ca18eb4e9088fa7648b103e9e3e8fb
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-06-24 17:15:32 +0100

    Hope it's good

--- screens/MainMenu.gd
+++ screens/MainMenu.gd
@@ -5,6 +5,12 @@ onready var options = $OptionsMenu
 onready var extra = $ExtraMenu
 onready var password = $PasswordMenu
 
+#1ST LETTERS IN codes NAMES SHOULD BE LOWER CASE AND LETTERS, NOT NUMBERS
+#BECAUSE FUNC NAMES CANT START WITH NUMBERS
+var codes := ["tutor1", "tutor2", "tutor3",
+				"c4rr13", "k4tr1n", "m3lv1n",
+				"osc4r0", "p33tt3", "ang3l4"]
+
 func _ready():
 	TranslationServer.set_locale("en")
 
@@ -74,3 +80,38 @@ func _on_PasswordButton_pressed():
 
 func _on_BackFromOptions3_pressed():
 	show_and_hide(menu, password)
+
+
+func _on_LineEdit_text_entered(keyword):
+	if !keyword in codes:
+		$PasswordMenu/LineEdit.clear()
+		return
+	$PasswordMenu/LineEdit.clear()
+	call(keyword)
+
+func tutor1():
+	SceneManager.change_scene("level_transition_effect", "res://src/levels/LevelTutorial1.tscn")
+
+func tutor2():
+	SceneManager.change_scene("level_transition_effect", "res://src/levels/LevelTutorial2.tscn")
+
+func tutor3():
+	SceneManager.change_scene("level_transition_effect", "res://src/levels/LevelTutorial3.tscn")
+
+func c4rr13():
+	SceneManager.change_scene("level_transition_effect", "res://src/levels/LevelTemplate.tscn")
+
+func k4tr1n():
+	SceneManager.change_scene("level_transition_effect", "res://src/levels/LevelTemplate2.tscn")
+
+func m3lv1n():
+	SceneManager.change_scene("level_transition_effect", "res://src/levels/LevelTemplate3.tscn")
+
+func osc4r0():
+	SceneManager.change_scene("level_transition_effect", "res://src/levels/LevelTemplate4.tscn")
+
+func p33tt3():
+	SceneManager.change_scene("level_transition_effect", "res://src/levels/LevelTemplate5.tscn")
+
+func ang3l4():
+	SceneManager.change_scene("level_transition_effect", "res://src/levels/LevelTemplate6.tscn")
