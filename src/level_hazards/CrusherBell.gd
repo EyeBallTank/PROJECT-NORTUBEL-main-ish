22dffc3c51a10ff83c0d09dacb722f94028f9ead
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-06-25 15:43:11 +0100

    not perfect but still

--- src/level_hazards/CrusherBell.gd
+++ src/level_hazards/CrusherBell.gd
@@ -7,6 +7,11 @@ var state = HOVER
 onready var start_position = global_position
 onready var raycast = $RayCast2D
 onready var sound = $AudioStreamPlayer
+onready var sprite = $AnimatedSprite
+onready var timer = $Timer
+
+func _ready():
+	sound.set_volume_db(-80)
 
 func _physics_process(delta):
 	match state:
@@ -19,17 +24,31 @@ func hover_state():
 	state = FALL
 
 func fall_state(delta):
+	sprite.animation = "falling"
 	position.y += 1000 * delta
 	if raycast.is_colliding():
 		var collision_point = raycast.get_collision_point()
 		position.y = collision_point.y
 		state = LAND
+		sound.play()
+		timer.start(1.0)
 
 func land_state():
-	sound.play()
-	state = RISE
+	if timer.time_left == 0:
+		state = RISE
 
 func rise_state(delta):
+	sprite.animation = "rising"
 	position.y = move_toward(position.y, start_position.y, 100 * delta)
 	if position.y == start_position.y:
 		state = HOVER
+
+
+func _on_SoundProximity_body_entered(body):
+	if body.name == "Player":
+		sound.set_volume_db(-3.736) 
+
+
+func _on_SoundProximity_body_exited(body):
+	if body.name == "Player":
+		sound.set_volume_db(-80) 
