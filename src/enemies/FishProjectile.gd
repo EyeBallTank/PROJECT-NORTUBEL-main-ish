1ac89efcc7382988a167f8e36391939ce93c4b7d
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-06-26 19:44:57 +0100

    getting there but i want to make it go to the other side

--- src/enemies/FishProjectile.gd
+++ src/enemies/FishProjectile.gd
@@ -2,14 +2,14 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 var direction = Vector2.RIGHT
-
+var speed = 30
 
 func _ready():
 	pass
 
 func _physics_process(delta):
 	if is_on_wall():
-		queue_free()
-#	velocity = direction * 1000
-	move_and_slide(direction, Vector2.UP)
+		pass
+	velocity.x = speed * 10
+	move_and_slide(velocity, Vector2.UP)
 
