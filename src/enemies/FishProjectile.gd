feadac69df92b226528f89af88d491a097064446
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-06-26 17:58:39 +0100

    bit of an update

--- src/enemies/FishProjectile.gd
+++ src/enemies/FishProjectile.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
-var velocity = Vector2(0, 0)
+var velocity = Vector2.ZERO
 var direction = Vector2.RIGHT
+
 
 func _ready():
 	pass
@@ -9,6 +10,6 @@ func _ready():
 func _physics_process(delta):
 	if is_on_wall():
 		queue_free()
-	velocity = direction * 1000
+#	velocity = direction * 1000
 	move_and_slide(direction, Vector2.UP)
 
