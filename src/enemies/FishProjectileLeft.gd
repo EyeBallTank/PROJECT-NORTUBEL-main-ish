e3faa21de87e17b7bade366d9c5a07b081ec2cce
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-06-26 21:31:48 +0100

    trying a different method

---
+++ src/enemies/FishProjectileLeft.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+var direction = Vector2.RIGHT
+var speed = 25
+onready var animation = $AnimationPlayer
+
+func _ready():
+	animation.play("RESET")
+
+func _physics_process(delta):
+	rotation_degrees += -7
+	if is_on_wall():
+		animation.play("dying")
+	velocity.x = speed * -10
+	move_and_slide(velocity, Vector2.UP)
+
+func die():
+	queue_free()
