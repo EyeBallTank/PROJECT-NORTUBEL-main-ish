a09e79683462f0d43c0a056c0cb480df623d8ebe
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-06-27 16:41:58 +0100

    new export vars for props to make them more versatile

--- src/level_props/LevelProp.gd
+++ src/level_props/LevelProp.gd
@@ -3,7 +3,12 @@ tool
 
 onready var sprite = $Sprite
 export var propname = "nortprop blue chimney"
-
+export var flipstate_h = false
+export var flipstate_v = false
+export var newrotation = 0
 
 func _ready():
 	sprite.animation = propname
+	sprite.flip_h = flipstate_h
+	sprite.flip_v = flipstate_v
+	sprite.global_rotation = newrotation
