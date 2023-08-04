189c12cbb5c83657c9b8739785318d4302fa65b0
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-08-04 23:35:56 +0100

    more work on the clown

--- src/npc_actors/brink npc/Bonka NPC.gd
+++ src/npc_actors/brink npc/Bonka NPC.gd
@@ -8,6 +8,7 @@ var honk = false
 onready var audioplayer = $AudioStreamPlayer
 onready var dialogue = $CanvasLayer
 onready var maintext = $CanvasLayer/NinePatchRect/Dialogue
+onready var sprite = $Sprite
 export(String) var customtext = "BONKA_TALK_1"
 export(String) var customtext2 = "BONKA_HONK"
 export(String) var customtext3 = "BONKA_HUG"
@@ -16,6 +17,7 @@ export(String) var customtext4 = "BONKA_HURT"
 func _ready():
 	maintext.text = str (customtext)
 	dialogue.visible = false
+	sprite.play("Idle")
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
@@ -23,12 +25,15 @@ func _physics_process(delta):
 	if honk == true:
 		if Input.is_action_pressed("kickball") and Input.is_action_just_pressed("standstill"):
 			maintext.text = str (customtext2)
+			sprite.play("Honked")
 			audioplayer.play()
 			yield(audioplayer, "finished")
 		if Input.is_action_just_pressed("followme"):
 			maintext.text = str (customtext3)
+			sprite.play("Hugged")
 		if Input.is_action_just_pressed("attack"):
 			maintext.text = str (customtext4)
+			sprite.play("Knife")
 
 func _on_TalktoArea_body_entered(body):
 	if body.name == "Player":
@@ -40,3 +45,4 @@ func _on_TalktoArea_body_exited(body):
 		dialogue.visible = false
 		honk = false
 		maintext.text = str (customtext)
+		sprite.play("Idle")
