153bd2990dd72800c72a122089a93fd103411e73
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-09-07 23:55:00 +0100

    adding images to the tutorial books

--- src/npc_actors/BookInfo.gd
+++ src/npc_actors/BookInfo.gd
@@ -3,12 +3,15 @@ extends KinematicBody2D
 onready var dialoguebox = $CanvasLayer
 onready var maintext = $CanvasLayer/NinePatchRect/Dialogue
 export(String) var customtext = "BOOKHINT_1"
+export var boardname = "board 1"
+onready var sprite = $CanvasLayer/AnimatedSprite
 
 #This is a basic implementation
 #May need JSON files and Arrays in the future
 
 func _ready():
 	maintext.text = str (customtext)
+	sprite.animation = boardname
 #	customtext = maintext.text
 	dialoguebox.visible = false
 	pass
