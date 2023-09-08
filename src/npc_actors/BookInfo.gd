5a1d2175eecb78d37f77219d6f171349c31ff688
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-09-08 23:51:36 +0100

    still need work

--- src/npc_actors/BookInfo.gd
+++ src/npc_actors/BookInfo.gd
@@ -6,6 +6,7 @@ export(String) var customtext = "BOOKHINT_1"
 export var boardname = "board 1"
 onready var sprite = $CanvasLayer/AnimatedSprite
 
+var test = false
 #This is a basic implementation
 #May need JSON files and Arrays in the future
 
@@ -14,14 +15,20 @@ func _ready():
 	sprite.animation = boardname
 #	customtext = maintext.text
 	dialoguebox.visible = false
-	pass
+
+func _physics_process(delta):
+	if test == true:
+		if Input.is_action_just_pressed("kickball"):
+			dialoguebox.visible = true
+	elif test == false:
+		dialoguebox.visible = false
 
 
 func _on_TalktoArea_body_entered(body):
 	if body.name == "Player":
-		dialoguebox.visible = true
+			test = true
 
 
 func _on_TalktoArea_body_exited(body):
 	if body.name == "Player":
-		dialoguebox.visible = false
+		test = false
