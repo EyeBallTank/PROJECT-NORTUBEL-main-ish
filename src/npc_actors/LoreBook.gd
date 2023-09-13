df3de51180c5fbd8230664a32662a8d4048d8814
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-09-13 23:11:32 +0100

    "lore book" test

---
+++ src/npc_actors/LoreBook.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+onready var dialoguebox = $CanvasLayer
+onready var maintext = $CanvasLayer/NinePatchRect/Dialogue
+export(String) var customtext = "BOOKHINT_1"
+onready var basicmessage = $Basicmessage
+
+var test = false
+
+func _ready():
+	maintext.text = str (customtext)
+	dialoguebox.visible = false
+	basicmessage.visible = false
+
+func _physics_process(_delta):
+	if test == true:
+		if Input.is_action_just_pressed("standstill") and dialoguebox.visible == false:
+			dialoguebox.visible = true
+	elif test == false:
+		dialoguebox.visible = false
+	if Input.is_action_just_pressed("runaway"):
+		test = false
+
+func _on_TalktoArea_body_entered(body):
+	if body.name == "Player":
+		basicmessage.visible = true
+		test = true
+
+func _on_TalktoArea_body_exited(body):
+	if body.name == "Player":
+		basicmessage.visible = false
+		test = false
