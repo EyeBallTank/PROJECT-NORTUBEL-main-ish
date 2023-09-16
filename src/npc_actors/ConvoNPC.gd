256300d0ac8409e71b82b78e085c55da460a4bd6
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-09-16 11:49:14 +0100

    hold on

--- src/npc_actors/ConvoNPC.gd
+++ src/npc_actors/ConvoNPC.gd
@@ -5,8 +5,12 @@ var velocity = Vector2.ZERO
 
 onready var dialoguebox = $CanvasLayer
 onready var maintext = $CanvasLayer/NinePatchRect/Dialogue
-export(String) var customtext = "BLABLA"
-export(String) var customname = "NAME"
+export(String) var customtext1 = "BLABLA"
+export(String) var customtext2 = "YESYES"
+export(String) var customtext3 = "NONO"
+export(String) var customtext4 = "OKOK"
+export(String) var customname1 = "Carrie"
+export(String) var customname2 = "Katrin"
 onready var mainsprite = $AnimatedSprite
 export var spritename = "carrie katrin"
 onready var basicmessage = $basicmessage
@@ -15,7 +19,7 @@ var test = false
 
 func _ready():
 	mainsprite.animation = spritename
-	maintext.text = str (customtext)
+	maintext.text = str (customtext1)
 	dialoguebox.visible = false
 	basicmessage.visible = false
 
