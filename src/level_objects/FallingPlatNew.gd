613cdac8347fcdaa399713b6d6affc86cbe527e1
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-09-28 18:05:02 +0100

    fall plat wip

--- src/level_objects/FallingPlatNew.gd
+++ src/level_objects/FallingPlatNew.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 onready var animationplayer = $AnimationPlayer
-
+onready var sprite = $Platform/Sprite
+export var spritename = "nortubel fallplat"
 
 func _ready():
-	pass 
-
+	sprite.animation = spritename
 
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
