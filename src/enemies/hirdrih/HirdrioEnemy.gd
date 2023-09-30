08e47f5609b8c3db82de04a06e1b10e20fdea21e
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-09-30 19:11:51 +0100

    i guess its finished for now

--- src/enemies/hirdrih/HirdrioEnemy.gd
+++ src/enemies/hirdrih/HirdrioEnemy.gd
@@ -1,13 +1,28 @@
 extends KinematicBody2D
 
 var velocity = Vector2(200, 200)
-
+onready var animation = $AnimationPlayer
 
 func _ready():
-	pass
+	animation.play("RESET")
 
 func _physics_process(delta):
 
 	var collision_info = move_and_collide(velocity * delta)
 	if collision_info:
 		velocity = velocity.bounce(collision_info.normal)
+
+func die():
+	queue_free()
+
+
+func _on_HirdrioHurtbox_body_entered(body):
+	if body.name == "SoccerBall":
+		animation.play("dying")
+
+
+func _on_HirdrioHurtbox_area_entered(area):
+	if area.name == "PlayerMelee":
+		animation.play("dying")
+	if area.name == "EnemyCrusher":
+		animation.play("dying")
