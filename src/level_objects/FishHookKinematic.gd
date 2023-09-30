7bde939e0725efe8ef46ea51be5ea24630dc76fe
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-09-30 16:50:23 +0100

    gotta make sure it's all safe

--- src/level_objects/FishHookKinematic.gd
+++ src/level_objects/FishHookKinematic.gd
@@ -1,4 +1,11 @@
 extends Node2D
 
+onready var anchorsprite = $Sprite
+export var anchorspritename = "nortubel anchor"
+onready var hooksprite = $FishHookWithWire/Sprite
+export var hookspritename = "nortubel hook"
+
+
 func _ready():
-	pass 
+	anchorsprite.animation = anchorspritename
+	hooksprite.animation = hookspritename
