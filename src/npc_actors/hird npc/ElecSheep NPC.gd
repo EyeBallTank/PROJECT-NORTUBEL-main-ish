77000ad8a9126368c9076bb6ab6b42a38521a1e8
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-10-02 21:56:50 +0100

    so far

---
+++ src/npc_actors/hird npc/ElecSheep NPC.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+export var gravity: = 2960
+var velocity = Vector2.ZERO
+
+onready var dialogue = $CanvasLayer
+onready var maintext = $CanvasLayer/NinePatchRect/Dialogue
+onready var sprite = $Sprite
+export(String) var customtext = "Beeeeehh"
+
+func _ready():
+	maintext.text = str (customtext)
+	dialogue.visible = false
+	sprite.play("MAINSTATE")
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+
+func _on_TalktoArea_body_entered(body):
+	if body.name == "Player":
+		dialogue.visible = true
+		sprite.play("Otherstate")
+		
+func _on_TalktoArea_body_exited(body):
+	if body.name == "Player":
+		dialogue.visible = false
+		sprite.play("MAINSTATE")
