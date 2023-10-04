3cd5dea094ff75e8b768cb1b2042903072f236c0
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-10-04 19:43:55 +0100

    let them be for now

--- src/level prefabs/hird prefab/Hird Popup ad.gd
+++ src/level prefabs/hird prefab/Hird Popup ad.gd
@@ -1,10 +1,16 @@
 extends KinematicBody2D
 
+var velocity = Vector2(400, 400)
 onready var animation = $AnimationPlayer
 
 func _ready():
 	animation.play("default")
 
+func _physics_process(delta):
+
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		velocity = velocity.bounce(collision_info.normal)
 
 func die():
 	queue_free()
