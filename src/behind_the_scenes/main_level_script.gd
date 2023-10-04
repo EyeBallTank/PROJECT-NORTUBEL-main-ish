589e3cc79c2e5084a98da2fb22c71fbc788bedb5
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-10-04 11:56:10 +0100

    is this okay?

--- src/behind_the_scenes/main_level_script.gd
+++ src/behind_the_scenes/main_level_script.gd
@@ -1,12 +1,12 @@
 extends Node
 class_name main_level_script 
 
-func _tree_entered():  
+#func _tree_entered():  
 	#Send all new nodes to register_node()
-	get_tree().connect("node_added", self, "register_node") 
+#	get_tree().connect("node_added", self, "register_node") 
 
 
-func register_node(node:Node):
+#func register_node(node:Node):
 	#Here is where you can add more "if" statements to filter which nodes are affected  
-	if node.owner == null:
-		node.owner = get_tree().current_scene
+#	if node.owner == null:
+#		node.owner = get_tree().current_scene
