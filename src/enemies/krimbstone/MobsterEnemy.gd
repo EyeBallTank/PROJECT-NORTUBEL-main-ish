d6ca89d2e56a71f99fd2bb9ed131c3cd4b590070
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-11-13 23:09:24 +0000

    still nothing

--- src/enemies/krimbstone/MobsterEnemy.gd
+++ src/enemies/krimbstone/MobsterEnemy.gd
@@ -4,11 +4,11 @@ export var gravity: = 29600
 
 var direction = Vector2.RIGHT
 var velocity = Vector2.ZERO
-var knockback_dir = 1
+
 onready var sprite: = $AnimatedSprite
 onready var floordetect = $Floordetect
 onready var eyes = $Eyes
-onready var eyescollosion = $Eyes/CollisionShape2D
+#onready var eyescollosion = $Eyes/CollisionShape2D
 onready var animation = $AnimationPlayer
 onready var sound = $AudioStreamPlayer
 onready var timer = $Timer
@@ -32,28 +32,30 @@ func _ready():
 func _physics_process(delta):
 	match state:
 		MOVING:
-			if canmove == true:
-				detect_turn_around()
-				sprite.play("Running")
-				var found_wall = is_on_wall()
-				if found_wall:
-					direction *= -1
-					scale.x = -scale.x
-				velocity = direction * 160
-				velocity.y += gravity * delta
-				move_and_slide(velocity, Vector2.UP)
-			elif canmove == false:
-				velocity = 0
-			if canattack == true:
-				sprite.play("Attack")
-				attack()
-			elif canattack == false:
-				pass
+			if see_to_attack():
+				state = SHOOT
+
+			detect_turn_around()
+			sprite.play("Running")
+			var found_wall = is_on_wall()
+			if found_wall:
+				direction *= -1
+				scale.x = -scale.x
+			velocity = direction * 160
+			velocity.y += gravity * delta
+			move_and_slide(velocity, Vector2.UP)
+
+
+		SHOOT:
+			velocity = 0
+			sprite.play("Attack")
+			attack()
+			timer.start(0.5)
+			if timer.time_left == 0:
+				state = MOVING
 
 		DEAD:
 			animation.play("Dying.")
-			canmove = false
-			canattack = false
 
 func detect_turn_around():
 	if not floordetect.is_colliding() and is_on_floor():
@@ -64,27 +66,31 @@ func detect_turn_around():
 func die():
 	queue_free()
 
+func see_to_attack():
+	if not eyes.is_colliding(): return false
+	var collider = eyes.get_collider()
+	if not collider is PlayerMain: return false
+	return true
+
+
 func attack():
 	var projectile = Bullet.instance()
 	projectile.global_position = gunhole.global_position
 	get_tree().get_root().add_child(projectile)
 	sprite.play("Attack")
 	timer.start(0.5)
-	if timer.time_left == 0:
-		canattack = false
-		canmove = true
 
-func _on_Eyes_body_entered(body):
-	if body.is_in_group("protagonists"):
-		sound.play()
-		canattack = true
-		canmove = false
+#func _on_Eyes_body_entered(body):
+#	if body.is_in_group("protagonists"):
+#		sound.play()
+#		canattack = true
+#		canmove = false
 #		attack()
 
-func _on_Eyes_body_exited(body):
-	if body.is_in_group("protagonists"):
-		canattack = false
-		canmove = true
+#func _on_Eyes_body_exited(body):
+#	if body.is_in_group("protagonists"):
+#		canattack = false
+#		canmove = true
 #		eyes.set_deferred("monitoring", true)
 
 func _on_EnemyHurtbox_area_entered(area):
