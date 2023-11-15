dff49f78e49778ad43057d718808d940687a79f7
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-11-15 19:17:16 +0000

    save

--- src/enemies/FishProjectile.gd
+++ src/enemies/FishProjectile.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 var direction = Vector2.RIGHT
 var speed = 25
-var spinning = 7
+#var spin_direction = Vector2.RIGHT
 onready var animation = $AnimationPlayer
 
 func _ready():
@@ -16,6 +16,7 @@ func _physics_process(delta):
 		animation.play("dying")
 #	velocity.x = speed * 10
 	move_and_slide(velocity, Vector2.UP)
+#	rotation_degrees += velocity * delta * spin_direction
 
 func die():
 	queue_free()
