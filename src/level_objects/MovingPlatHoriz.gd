66cbf035dd37f6074c07173fcfc05c9ee595c41d
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-11-20 17:53:34 +0000

    anyway

--- src/level_objects/MovingPlatHoriz.gd
+++ src/level_objects/MovingPlatHoriz.gd
@@ -3,5 +3,9 @@ extends Node2D
 onready var animationplayer = $AnimationPlayer
 export var animation = "HorizontalPlat"
 
+onready var sprite = $Platform/Sprite
+export var spritename = "nortubel movplathoriz"
+
 func _ready():
 	animationplayer.play(animation)
+	sprite.animation = spritename
