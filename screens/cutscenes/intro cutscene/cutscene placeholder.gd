21abf87bcb418e6434ca4430c082bb70deb79060
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-11-26 22:46:06 +0000

    future plans

--- screens/cutscenes/intro cutscene/cutscene placeholder.gd
+++ screens/cutscenes/intro cutscene/cutscene placeholder.gd
@@ -81,3 +81,46 @@ func cutscene_over():
 #CUTSCENE_6_TALK_20/Everybody, listen up! This path leads to an escape route that should keep you safe while I take care of Phineas' men./Malta, ouvem! Este caminho leva a uma rota de fuga que deverá manter-vos seguros enquanto eu cuido dos homens de Phineas.
 #CUTSCENE_6_TALK_21/But in case there's a few rats in the sewers, Stella should inform you about the traps I've set up./Mas em caso que haja alguns ratos nos esgotos, Stella deve informar-vos sobre as armadilhas que montei.
 #CUTSCENE_6_TALK_22/Don't worry, Booker. We can handle this situation./Não te preocupes, Booker. Nós podemos lidar com esta situação.
+
+
+#Stella: We made it!
+#Carrie: Feels weird that we took the lives of others, even if they were bad people.
+#Stella: Well, I felt the same way. Even I still feel weird about it.
+#Katrin: Booker, are you alright?
+#Booker: I got almost all of them... except for that coward Phineas. Of course he ran away.
+#Melvin: By the way, Stella, we could get you out of here.
+#Melvin: See this box? It's how we got here and it could get us out!
+#A portal shows up.
+#Stella: So this must be the same box from before.
+#Stella: Will we go back to Nortubel?
+#Pete: It looks like Nortubel but it's a weird place.
+#Oscar: Yeah, there's flying buildings and giant geckos.
+#Melvin: But Carrie's grandma could be getting close to take us back home!
+#Stella: Well, if that's what you say...
+#Booker: You should go.
+#Stella: Booker, but why?
+#Booker: Girl, I can still you're still not used to this Hellscape and maybe it's for the best.
+#Booker: If you get to used to things here, you might lose it and stumble upon a worse fate than death.
+#Booker: Even I wonder if it could happen to me, but you have a chance to return to a better place.
+#Stella: Is it wrong that I hope to see you again?
+#Booker: I warned you not to love me too much... but you did bring back a part of me I thought died years ago.
+#Booker: Somewhere in heaven, a man could rejoin his loved ones... had he not became a shadow participating on a war against demons.
+#Stella: Well, I'll miss you Booker. Thank you for everything.
+#She leaves with the 6 others.
+#Booker: Maybe if Veronica was still alive, you 2 could be friends. In fact, she could have been much older. How time passes...
+#Booker: Where was i?
+#Booker returns to his base, while Benjamin is tied to a chair. Booker prepares for a violent interrogation.
+#Meanwhile at Nortubel
+#Stella: What happened to our home?
+#Katrin: We're not even sure if it is Nortubel. For what we know, this could be a world someone created based on our home.
+#Zarina: But it's not just your country that has its own copy dimensions.
+#Katrin: Who are you, miss?
+#Zarina: My name is Zarina Liefdei and I'm a sorceress from Auliswaria, but I moved to Nortubel some time.
+#Oscar: Are you lost like us, miss?
+#Zarina: At first I thought I wasn't, but it seems my powers aren't working as of now.
+#Carrie: I recognize you. You've met my grandma before, right?
+#Zarina: I'm a friend of Pamela and I heard she's trying to you all back home.
+#Melvin: That's true. We also got this magic box that bring us to other worlds.
+#Angela: We got to meet robots and a guy that kills bad people.
+#Zarina: I've heard of such device. You can unlock new worlds if you find its missing pieces, specially when you consider the rotation of each knob.
+#Zarina: But even then, there could be more to that box that is missing.
