b7774f9d02e39b92ecea2e7891fca62b0caa198d
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2023-12-04 17:05:57 +0000

    finally

--- Massag PhotoBooth.gd
+++ Massag PhotoBooth.gd
@@ -3,4 +3,9 @@ extends Node2D
 onready var animation = $AnimationPlayer
 
 func _ready():
-	pass 
+	animation.play("RESET") 
+
+
+func _on_Takeaphoto_body_entered(body):
+	if body.name == "Player":
+		animation.play("phototaken")
