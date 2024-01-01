41e822fbab21e26c7e08030daba5d2042834b1ea
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-01-01 17:26:16 +0000

    still nothing

--- src/enemies/masagcoav/BlackChicken.gd
+++ src/enemies/masagcoav/BlackChicken.gd
@@ -7,18 +7,21 @@ var velocity = Vector2.ZERO
 onready var eyes = $Eyes
 onready var dizzytimer = $DizzyTimer
 onready var impacttimer = $ImpactTimer
+onready var animationplayer = $AnimationPlayer
+
 
 enum {
 	MOVING,
 	RUNNING,
-	DIZZY,
+	DIZZYJUMP,
+	DIZZYIDLE,
 	DEAD
 }
 
 var state = MOVING
 
 func _ready():
-	pass
+	animationplayer.play("RESET")
 
 func _physics_process(delta):
 	match state:
@@ -33,39 +36,59 @@ func _physics_process(delta):
 			velocity = direction * 160
 			velocity.y += gravity * delta
 			move_and_slide(velocity, Vector2.UP)
-			gravity = 37600
+			gravity = 33600
 		RUNNING:
 			velocity = direction * 560
 			velocity.y += gravity * delta
 			move_and_slide(velocity, Vector2.UP)
-			gravity = 37600
+			gravity = 33600
 			var found_wall = is_on_wall()
 			if found_wall:
 				direction *= -1
 				scale.x = -scale.x
-				state = DIZZY
+				state = DIZZYJUMP
 				
-		DIZZY:
-			impacttimer.start(1)
-			velocity = direction * 50
+		DIZZYJUMP:
+
+			velocity = direction * 100
 			velocity.y += gravity * delta
 			move_and_slide(velocity, Vector2.UP)
-			gravity = 37600
+			gravity = 33600
 
+			impacttimer.start(1.0)
 			if impacttimer.time_left == 0:
-				velocity.x = 0
-				dizzytimer.start(2)
-				if dizzytimer.time_left == 0:
-					state = MOVING
+				state = DIZZYIDLE
+
+		DIZZYIDLE:
+			velocity.x = 0
+			velocity.y += gravity * delta
+			move_and_slide(velocity, Vector2.UP)
+			gravity = 33600
+
+			dizzytimer.start(2.0)
+			if dizzytimer.time_left == 0:
+				state = MOVING
+
+		DEAD:
+			animationplayer.play("dying")
 
 func see_to_attack():
 	if not eyes.is_colliding(): return false
 	var collider = eyes.get_collider()
 	if not collider.is_in_group("protagonists"): return false
-#	if not collider is StellaMain: return false
-#	if not collider is Companion: return false
 	return true
 
 func die():
 	queue_free()
 
+
+func _on_HurtBoxChicken_area_exited(area):
+	if area.name == "PlayerMelee":
+		state = DEAD
+	if area.name == "EnemyCrusher":
+		state = DEAD
+
+
+func _on_HurtBoxChicken_body_entered(body):
+	if body.name == "SoccerBall":
+		state = DEAD
