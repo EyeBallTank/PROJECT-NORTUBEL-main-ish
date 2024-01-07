e25e46ab69354b61a351bea6879d75f7fcdb6b5d
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-01-07 23:22:54 +0000

    so far

--- PossessedLantern.gd
+++ PossessedLantern.gd
@@ -4,13 +4,19 @@ export var speed = 120
 var comp_position
 var target_position
 onready var companion = get_parent().get_node("Companion")
+onready var sprite = $AnimatedSprite
+onready var animation = $AnimationPlayer
 
 enum {
 	IDLE,
-	ACTIVE
+	ACTIVE,
+	DEAD
 }
 
 var state = IDLE
+
+func _ready():
+	animation.play("RESET")
 
 func _physics_process(delta):
 	match state:
@@ -23,7 +29,26 @@ func _physics_process(delta):
 
 			if position.distance_to(comp_position) > 3:
 				move_and_slide(target_position * speed)
+			if companion.health == 0:
+				state = IDLE
+# Guess the 0 health detection part works?
+		DEAD:
+			animation.play("dying")
+
+func die():
+	queue_free()
 
 func _on_Vision_body_entered(body):
 	if body.name == "Companion":
 		state = ACTIVE
+
+
+func _on_enemyhurtbox_body_entered(body):
+	if body.name == "SoccerBall":
+		state = DEAD
+
+func _on_enemyhurtbox_area_entered(area):
+	if area.name == "PlayerMelee":
+		state = DEAD
+	if area.name == "EnemyCrusher":
+		state = DEAD
