3ab96b12ceb317deb6f58adacdd866f59caa9777
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-01-11 10:53:47 +0000

    so far

--- src/level prefabs/nort prefabs/NortFloatingTarget.gd
+++ src/level prefabs/nort prefabs/NortFloatingTarget.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 
 var velocity = Vector2(200, 200)
+var horizdirection = Vector2.RIGHT
+var vertidirection = Vector2.UP
 onready var animation = $AnimationPlayer
 
-#export var customstate = "DIAGONAL"
+export var customstate = 1
 
 enum {
 	DIAGONAL,
@@ -17,6 +19,13 @@ func _ready():
 	animation.play("RESET")
 
 func _physics_process(delta):
+	if customstate == 1:
+		state = DIAGONAL
+	elif customstate == 2:
+		state = HORIZONTAL
+	elif customstate == 3:
+		state = VERTICAL
+
 	match state:
 		DIAGONAL:
 			var collision_info = move_and_collide(velocity * delta)
@@ -24,10 +33,21 @@ func _physics_process(delta):
 				velocity = velocity.bounce(collision_info.normal)
 
 		HORIZONTAL:
-			pass
+			var found_wall = is_on_wall()
+			if found_wall:
+				horizdirection *= -1
+			velocity = horizdirection * 290
+			move_and_slide(velocity, Vector2.UP)
 
 		VERTICAL:
-			pass
+			var hitceiling = is_on_ceiling()
+			var hitfloor = is_on_floor()
+			if hitfloor:
+				vertidirection *= -1
+			if hitceiling:
+				vertidirection *= -1
+			velocity = vertidirection * 290
+			move_and_slide(velocity, Vector2.UP)
 
 
 func die():
