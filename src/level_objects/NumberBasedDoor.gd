a7a0017566219edf640a4b08680757d6c0bcaf65
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-01-20 12:41:03 +0000

    more

--- src/level_objects/NumberBasedDoor.gd
+++ src/level_objects/NumberBasedDoor.gd
@@ -1,15 +1,23 @@
 extends StaticBody2D
 
-
+onready var sprite = $NumberBasedDoor
+export var spritename = "default"
 export var idealnumber = 5
 var currentnumber = 0
 
+onready var animationplayer = $AnimationPlayer
+
 func _ready():
+	animationplayer.play("RESET")
 	Signals.connect("collectible_picked", self, "_number_goes_up")
+	sprite.animation = spritename
 
 func _number_goes_up():
 	currentnumber += 1
 
 func _physics_process(delta):
 	if currentnumber == idealnumber:
-		queue_free()
+		animationplayer.play("disappear")
+
+func die():
+	queue_free()
