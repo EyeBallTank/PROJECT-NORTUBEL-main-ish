7e2fd31e60faa2909229adc52f18f2ff75af745d
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-01-21 15:40:29 +0000

    seesaw handcar works

--- src/level prefabs/nort prefabs/NortSeesawTrain.gd
+++ src/level prefabs/nort prefabs/NortSeesawTrain.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 onready var sprite =  $KinematicBody2D/AnimatedSprite
 onready var animationplayer = $AnimationPlayer
+var playertouch = false
+var comptouch = false
+
 
 #NOTE: MAKE THE "PATH" LONGER
 #MAKE IT THAT EITHER BOTH CHARACTERS ARE NEEDED OR THE PLAYER USES AN INPUT (maybe both)
@@ -10,15 +13,32 @@ func _ready():
 	animationplayer.play("RESET")
 	sprite.play("default")
 
-
-
-func _on_CharacterDetect_body_entered(body):
-	if body.is_in_group("protagonists"):
-		animationplayer.play("moving")
-		sprite.play("moving")
-
-
-func _on_CharacterDetect_body_exited(body):
-	if body.is_in_group("protagonists"):
+func _physics_process(delta):
+	if playertouch == true and comptouch == true:
+		if Input.is_action_just_pressed("standstill"):
+			animationplayer.play("moving")
+			sprite.play("moving")
+		elif Input.is_action_just_pressed("runaway"):
+			animationplayer.stop(false)
+			sprite.play("default")
+	elif playertouch == false or comptouch == false:
 		animationplayer.stop(false)
 		sprite.play("default")
+
+func _on_CharacterDetect_body_entered(body):
+#	if body.is_in_group("protagonists"):
+#		animationplayer.play("moving")
+#		sprite.play("moving")
+	if body.name == "Player":
+		playertouch = true
+	if body.name == "Companion":
+		comptouch = true
+
+func _on_CharacterDetect_body_exited(body):
+#	if body.is_in_group("protagonists"):
+#		animationplayer.stop(false)
+#		sprite.play("default")
+	if body.name == "Player":
+		playertouch = false
+	if body.name == "Companion":
+		comptouch = false
