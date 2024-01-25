7da09f121e8a428dc81d0509f4d9a01dc7670d83
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-01-25 12:34:00 +0000

    nvm

--- src/level_objects/Soccel Goal.gd
+++ src/level_objects/Soccel Goal.gd
@@ -2,23 +2,17 @@ extends StaticBody2D
 tool
 
 onready var animationplayer = $AnimationPlayer
-export var sides = 1
 
 func _ready():
-	if sides == 1:
-		pass
-	elif sides == 2:
-		scale.x = -scale.x
 	$AnimationPlayer.play("RESET")
 	pass
+
 
 
 func _on_BallDetector_area_entered(area):
 	if area.name == "GoalDetector":
 		$AudioStreamPlayer.play()
 		animationplayer.play("ballreceived")
-#		yield(get_tree().create_timer(1), "timeout")
-#		queue_free()
 
 func disappear():
 	queue_free()
