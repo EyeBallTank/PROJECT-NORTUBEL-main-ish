91adb25a3b28b42e30898e40aed0606de6ede32a
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-02-19 15:45:09 +0000

    wait

--- src/enemies/greklovick/GrekAmmoDispenser.gd
+++ src/enemies/greklovick/GrekAmmoDispenser.gd
@@ -1,6 +1,28 @@
 extends KinematicBody2D
 
+export var gravity: = 23600
 
+var direction = Vector2.RIGHT
+var velocity = Vector2.ZERO
+
+onready var sprite: = $AnimatedSprite
+onready var floordetect = $Floordetect
+onready var eyes = $Eyes
+onready var animation = $AnimationPlayer
+onready var timer = $Timer
+onready var gunhole = $Position2D
+var canattack = false
+var canmove = true
+
+const Bullet = preload("res://src/enemies/greklovick/GrekGrenade.tscn")
+
+enum {
+	MOVING,
+	SHOOT,
+	DEAD
+}
+
+var state = MOVING
 
 func _ready():
 	pass
