b9941e3331e9be529eddfc4883269be630db9b26
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-02-19 19:45:06 +0000

    oops

--- src/enemies/greklovick/GrekAmmoDispenser.gd
+++ src/enemies/greklovick/GrekAmmoDispenser.gd
@@ -78,15 +78,20 @@ func return_to_move():
 
 func attack():
 
-	if direction == Vector2.RIGHT:
-		var grenade = Bullet.instance()
-		get_tree().get_root().add_child(grenade)
-		grenade.global_position = gunhole.global_position
+	var grenade = Bullet.instance()
+	get_tree().get_root().add_child(grenade)
+	grenade.global_position = gunhole.global_position
+#	grenade.linear_velocity = Vector2(direction * BALL_VELOCITY, 0)
+
+#	if direction == Vector2.RIGHT:
+#		var grenade = Bullet.instance()
+#		get_tree().get_root().add_child(grenade)
+#		grenade.global_position = gunhole.global_position
 #		grenade.linear_velocity = Vector2(direction * BALL_VELOCITY, 0)
-	elif direction == Vector2.LEFT:
-		var grenade = Bullet.instance()
-		get_tree().get_root().add_child(grenade)
-		grenade.global_position = gunhole.global_position
+#	elif direction == Vector2.LEFT:
+#		var grenade = Bullet.instance()
+#		get_tree().get_root().add_child(grenade)
+#		grenade.global_position = gunhole.global_position
 #		grenade.linear_velocity = Vector2(direction * BALL_VELOCITY, 0)
 	sprite.play("Attack")
 	timer.start(0.5)
