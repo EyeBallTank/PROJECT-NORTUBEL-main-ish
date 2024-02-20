15c42bf9e5ecd0624e8a66b210c0b27e0ac42d20
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-02-20 01:07:06 +0000

    nothing

--- src/enemies/greklovick/GrekScalper.gd
+++ src/enemies/greklovick/GrekScalper.gd
@@ -4,10 +4,12 @@ export var gravity: = 33600
 
 var direction = Vector2.RIGHT
 var velocity = Vector2.ZERO
+var JUMP_SPEED = 1450
+
 onready var eyes = $Eyes
 onready var animationplayer = $AnimationPlayer
 onready var sprites = $Sprites
-
+onready var Player = get_parent().get_node("Player")
 
 func _ready():
 	pass 
@@ -26,8 +28,10 @@ func _physics_process(delta):
 	match state:
 		MOVING:
 			sprites.play("moving")
-			if see_to_attack():
+			if see_to_attack() and is_on_floor():
 				state = JUMP
+			elif see_to_attack() and not is_on_floor():
+				pass
 			var found_wall = is_on_wall()
 			if found_wall:
 				direction *= -1
@@ -37,15 +41,29 @@ func _physics_process(delta):
 			move_and_slide(velocity, Vector2.UP)
 			gravity = 33600
 
-
 		JUMP:
 			sprites.play("jump")
+			velocity = direction * 560
+			velocity.y += gravity * delta
+			move_and_slide(velocity, Vector2.UP)
+			gravity = 33600
+			var found_wall = is_on_wall()
+			if found_wall:
+				do_a_jump()
+
 
 		EATING:
 			sprites.play("eating")
+			global_position = Player.global_position
 
 		DEAD:
 			sprites.play("dead")
+
+
+func do_a_jump():
+	velocity.y = -JUMP_SPEED
+	if velocity.y < 0:
+		velocity.y += 500
 
 func see_to_attack():
 	if not eyes.is_colliding(): return false
