ceec7e190052c0b5ea774891ef03b1fcac0a01ac
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-02-28 23:24:02 +0000

    wait

--- src/enemies/herrko/HerrkoRedGhalbeen.gd
+++ src/enemies/herrko/HerrkoRedGhalbeen.gd
@@ -11,6 +11,9 @@ onready var animation = $AnimationPlayer
 onready var timer = $Timer
 onready var arrowsource = $ArrowSource
 
+const Bullet = preload("res://src/level_hazards/herrko/GhalbeenArrow.tscn")
+
+
 enum {
 	MOVING,
 	ARROW,
@@ -20,4 +23,51 @@ enum {
 var state = MOVING
 
 func _ready():
-	pass 
+	animation.play("RESET")
+
+func _physics_process(delta):
+	match state:
+		MOVING:
+			if see_to_attack():
+				state = ARROW
+
+			sprite.play("moving")
+			var found_wall = is_on_wall()
+			if found_wall:
+				direction *= -1
+				scale.x = -scale.x
+			velocity = direction * 160
+			velocity.y += gravity * delta
+			move_and_slide(velocity, Vector2.UP)
+			gravity = 23600
+
+
+		ARROW:
+			velocity.x = 0
+			animation.play("Shoot")
+			timer.start(2)
+			if timer.time_left == 0:
+				state = MOVING
+			velocity.y += gravity * delta
+			move_and_slide(velocity, Vector2.UP)
+
+		DEAD:
+			animation.play("Dying")
+
+func return_to_move():
+	state = MOVING
+
+func die():
+	queue_free()
+
+func see_to_attack():
+	if not eyes.is_colliding(): return false
+	var collider = eyes.get_collider()
+	if not collider.is_in_group("protagonists"): return false
+	return true
+
+func attack():
+	var projectile = Bullet.instance()
+	projectile.global_position = arrowsource.global_position
+	get_tree().get_root().add_child(projectile)
+
