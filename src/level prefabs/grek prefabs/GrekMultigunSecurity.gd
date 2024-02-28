8ed179188456c05d1473ba6ca03e055d4d2a9722
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-02-28 13:18:26 +0000

    might be fixed

--- src/level prefabs/grek prefabs/GrekMultigunSecurity.gd
+++ src/level prefabs/grek prefabs/GrekMultigunSecurity.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-var honk = false
+#var honk = false
 onready var dialogue = $CanvasLayer
 onready var elecplace = $Position2D
 onready var animation = $AnimationPlayer
@@ -16,26 +16,26 @@ func _ready():
 	dialogue.visible = false
 	animation.play("RESET")
 
-func _physics_process(delta):
-	if honk == true:
-		if Input.is_action_pressed("kickball") and Input.is_action_just_pressed("standstill"):
-			animation.play("active")
-		if Input.is_action_just_pressed("followme"):
-			animation.play("active")
+#func _physics_process(delta):
+#	if honk == true:
+#		if Input.is_action_pressed("kickball") and Input.is_action_just_pressed("standstill"):
+#			animation.play("active")
+#		if Input.is_action_just_pressed("followme"):
+#			animation.play("active")
 #		if Input.is_action_just_pressed("attack"):
 #			animation.play("active")
 
 func _on_TalkToArea_body_entered(body):
 	if body.name == "Player":
 		dialogue.visible = true
-		honk = true
+#		honk = true
 #		attack()
 #		animation.play("active")
 
 func _on_TalkToArea_body_exited(body):
 	if body.name == "Player":
 		dialogue.visible = false
-		honk = false
+#		honk = false
 
 func attack():
 	Signals.emit_signal("grek_gun_active")
