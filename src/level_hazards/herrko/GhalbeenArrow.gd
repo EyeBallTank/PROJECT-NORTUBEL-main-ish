c3550906c6b105b7f152bc238860aaf1972f8f24
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-02-28 23:06:41 +0000

    save

---
+++ src/level_hazards/herrko/GhalbeenArrow.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+export var speed = 120
+var player_position
+var target_position
+onready var player = get_parent().get_node("Player")
+onready var animation = $AnimationPlayer
+
+
+func _ready():
+	animation.play("RESET")
+	Signals.connect("scene_has_changed", self, "therefore_I_Go")
+
+func _physics_process(delta):
+	player_position = player.position
+	target_position = (player_position - position).normalized()
+
+	if position.distance_to(player_position) > 3:
+		move_and_slide(target_position * speed)
+
+func die():
+	queue_free()
+
+func therefore_I_Go():
+	die()
+
+
+func _on_Touchplayer_body_entered(body):
+	if body.is_in_group("protagonists"):
+		animation.play("Dying")
+
+
+func _on_HurtArrow_area_entered(area):
+	if area.name == "PlayerMelee":
+		animation.play("Dying")
