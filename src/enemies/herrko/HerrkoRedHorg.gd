7561281a5c91aae379c6dc095243620009d95e34
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-02-29 12:45:22 +0000

    wip

--- src/enemies/herrko/HerrkoRedHorg.gd
+++ src/enemies/herrko/HerrkoRedHorg.gd
@@ -21,3 +21,50 @@ var state = MOVING
 
 func _ready():
 	pass 
+
+func _physics_process(delta):
+
+	match state:
+		MOVING:
+			if see_to_attack():
+				state = HAMMER
+
+			sprite.play("moving")
+			var found_wall = is_on_wall()
+			if found_wall:
+				direction *= -1
+				scale.x = -scale.x
+			velocity = direction * 160
+			velocity.y += gravity * delta
+			move_and_slide(velocity, Vector2.UP)
+
+		HAMMER:
+			velocity.x = 0
+			animation.play("HammerAttack")
+			timer.start(3)
+			if timer.time_left == 0:
+				state = MOVING
+			velocity.y += gravity * delta
+			move_and_slide(velocity, Vector2.UP)
+
+		DEAD:
+			animation.play("Dying")
+
+func die():
+	queue_free()
+
+func return_to_move():
+	state = MOVING
+
+func see_to_attack():
+	if not eyes.is_colliding(): return false
+	var collider = eyes.get_collider()
+	if not collider.is_in_group("protagonists"): return false
+	return true
+
+func attack():
+	pass
+
+func _on_Hurtbox_area_entered(area):
+	if area.name == "PlayerMelee":
+		state = DEAD
