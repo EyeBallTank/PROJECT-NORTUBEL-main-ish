36c5ea4d1560a709bc340bb3d1a0a1c845a0006c
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-03-05 13:33:13 +0000

    save

--- src/enemies/brinkaedea/BrinkRedJester.gd
+++ src/enemies/brinkaedea/BrinkRedJester.gd
@@ -1,14 +1,18 @@
 extends KinematicBody2D
 
+#ISSUES:
+#MUCH LIKE RED GHALBEEN. HE CAN'T TURN AROUND WHEN COLLIDING WITH WALLS
+
+
 export var gravity: = 3700
 
 var direction = Vector2.RIGHT
 var velocity = Vector2.ZERO
-var walk_speed = 90
+var walk_speed = 160
 
 onready var sprites = $AnimatedSprite
 onready var animation = $AnimationPlayer
-onready var raycast = $RayCast2D
+onready var eyes = $RayCast2D
 onready var timer = $Timer
 
 enum {
@@ -23,8 +27,6 @@ func _ready():
 	animation.play("RESET")
 
 func _physics_process(delta):
-
-
 
 	match state:
 		MOVING:
@@ -42,7 +44,7 @@ func _physics_process(delta):
 
 		SUMMON:
 			velocity.x = 0
-#			animation.play("Shoot")
+			animation.play("attack")
 			timer.start(2)
 			if timer.time_left == 0:
 				state = MOVING
@@ -50,14 +52,32 @@ func _physics_process(delta):
 			move_and_slide(velocity, Vector2.UP)
 
 		DEAD:
-			pass
+			animation.play("dying")
+			velocity.x = 0
+			velocity.y += gravity * delta
+			move_and_slide(velocity, Vector2.UP)
+
+func attack():
+	pass
 
 func see_to_attack():
-	pass
+	if not eyes.is_colliding(): return false
+	var collider = eyes.get_collider()
+	if not collider.is_in_group("protagonists"): return false
+#	if not collider is StellaMain: return false
+#	if not collider is Companion: return false
+	return true
 
 func return_to_move():
-	pass
+	state = MOVING
 
 func die():
 	queue_free()
 
+
+
+func _on_EnemyHurtbox_area_exited(area):
+	if area.name == "PlayerMelee":
+		state = DEAD
+	if area.name == "EnemyCrusher":
+		state = DEAD
