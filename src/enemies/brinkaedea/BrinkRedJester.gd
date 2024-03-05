a144b151f4220efac011b1df311e19fd45c15d2a
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-03-05 11:47:31 +0000

    save

--- src/enemies/brinkaedea/BrinkRedJester.gd
+++ src/enemies/brinkaedea/BrinkRedJester.gd
@@ -1,5 +1,22 @@
 extends KinematicBody2D
 
+export var gravity: = 3700
+
+var direction = Vector2.RIGHT
+var velocity = Vector2.ZERO
+
+onready var sprite = $AnimatedSprite
+onready var animation = $AnimationPlayer
+onready var raycast = $RayCast2D
+onready var timer = $Timer
+
+enum {
+	MOVING,
+	SUMMON,
+	DEAD
+}
+
+var state = MOVING
 
 func _ready():
-	pass
+	animation.play("RESET")
