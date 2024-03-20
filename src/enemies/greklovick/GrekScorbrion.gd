7f639e76a52fb0805cf6cf853bf218b1a491859c
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-03-20 10:04:14 +0000

    save

--- src/enemies/greklovick/GrekScorbrion.gd
+++ src/enemies/greklovick/GrekScorbrion.gd
@@ -79,9 +79,9 @@ func attack():
 	var grenade = Bullet.instance()
 	get_tree().get_root().add_child(grenade)
 	grenade.global_position = gunhole.global_position
-	if sprite.flip_h == true:
+	if sprite.flip_h == false:
 		grenade.velocity.x = grenade.speed * 10
-	elif sprite.flip_h == false:
+	elif sprite.flip_h == true:
 		grenade.velocity.x = grenade.speed * -10
 
 
