4e508c9443aa86be85adfc5292a5ffebba7ee99e
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-03-20 08:54:41 +0000

    save

--- src/enemies/greklovick/GrekScorbrion.gd
+++ src/enemies/greklovick/GrekScorbrion.gd
@@ -1,16 +1,58 @@
 extends KinematicBody2D
 
+export var gravity: = 23600
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var direction = Vector2.RIGHT
+var velocity = Vector2.ZERO
+var BALL_VELOCITY = 1200.0
 
+onready var sprite: = $AnimatedSprite
+onready var floordetect = $Floordetect
+onready var eyes = $Eyes
+onready var animation = $AnimationPlayer
+onready var timer = $Timer
+onready var gunhole = $Position2D
+var canattack = false
+var canmove = true
 
-# Called when the node enters the scene tree for the first time.
+#const Bullet = preload("res://src/enemies/greklovick/GrekGrenade.tscn")
+
+enum {
+	MOVING,
+	SHOOT,
+	DEAD
+}
+
+var state = MOVING
+
 func _ready():
-	pass # Replace with function body.
+	pass
 
+func _physics_process(delta):
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	match state:
+		MOVING:
+			pass
+
+		SHOOT:
+			pass
+
+		DEAD:
+			pass
+
+func detect_turn_around():
+	if not floordetect.is_colliding() and is_on_floor():
+		direction *= -1
+		scale.x = -scale.x
+
+func die():
+	queue_free()
+
+func see_to_attack():
+	if not eyes.is_colliding(): return false
+	var collider = eyes.get_collider()
+	if not collider.is_in_group("protagonists"): return false
+	return true
+
+func return_to_move():
+	state = MOVING
