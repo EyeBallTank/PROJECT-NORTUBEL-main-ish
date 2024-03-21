8b14553a145b1a78fb5b294eb38ef41224f10f38
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-03-21 20:28:20 +0000

    save even more

--- src/enemies/herrko/TsoovukotKnight.gd
+++ src/enemies/herrko/TsoovukotKnight.gd
@@ -1,5 +1,92 @@
 extends KinematicBody2D
 
+export var gravity: = 23600
+
+var direction = Vector2.RIGHT
+var velocity = Vector2.ZERO
+
+onready var sprite: = $AnimatedSprite
+onready var floordetect = $Floordetect
+onready var eyes = $Eyes
+
+onready var animation = $AnimationPlayer
+onready var timer = $Timer
+onready var shieldspawn = $ShieldSpawn
+onready var meleeattack = $MeleeAttack
+var canattack = false
+var canmove = true
+
+#const Bullet = preload("res://src/enemies/lahiurn/EygzouhlFire.tscn")
+#const FloorFire = preload("res://src/enemies/lahiurn/EygzouhlFloorFire.tscn")
+#const BulletLeft = preload("res://src/enemies/lahiurn/EygzouhlFireLeft.tscn")
+
+enum {
+	MOVING,
+	SHOOT,
+	DEAD
+}
+
+var state = MOVING
 
 func _ready():
-	pass 
+	animation.play("RESET")
+
+func _physics_process(delta):
+	match state:
+		MOVING:
+			detect_turn_around()
+			sprite.play("Running")
+			var found_wall = is_on_wall()
+			if found_wall:
+				direction *= -1
+				scale.x = -scale.x
+			velocity = direction * 200
+			velocity.y += gravity * delta
+			move_and_slide(velocity, Vector2.UP)
+			gravity = 37600
+
+		SHOOT:
+
+			velocity.x = 0
+			animation.play("Shoot")
+			timer.start(3)
+			if timer.time_left == 0:
+				state = MOVING
+			velocity.y += gravity * delta
+			move_and_slide(velocity, Vector2.UP)
+
+		DEAD:
+			animation.play("Dying.")
+
+func shield():
+	pass
+
+func attack():
+	pass
+
+func detect_turn_around():
+	if not floordetect.is_colliding() and is_on_floor():
+		direction *= -1
+#		sprite.flip_h = direction.x < 0
+		scale.x = -scale.x
+
+func die():
+	queue_free()
+
+func see_to_attack():
+	if not eyes.is_colliding(): return false
+	var collider = eyes.get_collider()
+	if not collider.is_in_group("protagonists"): return false
+#	if not collider is StellaMain: return false
+#	if not collider is Companion: return false
+	return true
+
+func return_to_move():
+	state = MOVING
+
+
+func _on_EnemyHurtbox_area_entered(area):
+	if area.name == "PlayerMelee":
+		state = DEAD
+	if area.name == "EnemyCrusher":
+		state = DEAD
