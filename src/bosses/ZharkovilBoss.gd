5fe56e76099bfbf76a1e3ad25585556c8c800515
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-03-23 08:59:54 +0000

    save

--- src/bosses/ZharkovilBoss.gd
+++ src/bosses/ZharkovilBoss.gd
@@ -1,5 +1,28 @@
 extends KinematicBody2D
 
+var direction = Vector2.RIGHT
+var velocity = Vector2.ZERO
+
+onready var sprite: = $AnimatedSprite
+onready var eyes = $Eyes
+onready var animation = $AnimationPlayer
+onready var timer = $Timer
+onready var gunhole = $Gunhole
+onready var orbsource = $OrbSource
+var phase = 1
+
+#const ShotgunBlast = preload("res://src/enemies/greklovick/GrekShotgunPellets.tscn")
+#const ShotgunBlastLeft = preload("res://src/enemies/greklovick/GrekShotgunPelletsLeft.tscn")
+#const Bomb = preload("res://src/enemies/greklovick/TemporaryExplosion.tscn")
+
+enum {
+	MOVING,
+	SHOOT,
+	HURT,
+	DEAD
+}
+
+var state = MOVING
 
 func _ready():
-	pass
+	animation.play("RESET")
