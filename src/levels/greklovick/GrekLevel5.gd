5f15a1e92b61006f44d8dc19599f19bcedea76eb
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-03-23 22:46:43 +0000

    almost?

--- src/levels/greklovick/GrekLevel5.gd
+++ src/levels/greklovick/GrekLevel5.gd
@@ -51,30 +51,49 @@ func switch_pressed():
 
 
 func _on_ButtonPress6_body_entered(body):
-	if body.is_in_group("protagonists"):
+	if body.is_in_group("protagonists") and switchfive == false:
 		switchsix = true
 		shieldgen6.animation = "off"
+		switch_pressed()
+	elif body.is_in_group("protagonists") and switchfive == true:
+		animation.play("yiegourattacksthree")
+		send_a_signal()
 
 func _on_ButtonPress5_body_entered(body):
-	if body.is_in_group("protagonists"):
+	if body.is_in_group("protagonists") and switchsix == false:
 		switchfive = true
 		shieldgen5.animation = "off"
+		switch_pressed()
+	elif body.is_in_group("protagonists") and switchsix == true:
+		animation.play("yiegourattacksthree")
+		send_a_signal()
 
 func _on_ButtonPress4_body_entered(body):
-	if body.is_in_group("protagonists"):
+	if body.is_in_group("protagonists") and switchone == false:
 		switchfour = true
 		shieldgen4.animation = "off"
 		switch_pressed()
+	elif body.is_in_group("protagonists") and switchone == true:
+		animation.play("yiegourattacksone")
+		send_a_signal()
 
 func _on_ButtonPress3_body_entered(body):
-	if body.is_in_group("protagonists"):
+	if body.is_in_group("protagonists") and switchtwo == false:
 		switchthree = true
 		shieldgen3.animation = "off"
+		switch_pressed()
+	elif body.is_in_group("protagonists") and switchtwo == true:
+		animation.play("yiegourattackstwo")
+		send_a_signal()
 
 func _on_ButtonPress2_body_entered(body):
-	if body.is_in_group("protagonists"):
+	if body.is_in_group("protagonists") and switchthree == false:
 		switchtwo = true
 		shieldgen2.animation = "off"
+		switch_pressed()
+	elif body.is_in_group("protagonists") and switchthree == true:
+		animation.play("yiegourattackstwo")
+		send_a_signal()
 
 func _on_ButtonPress1_body_entered(body):
 	if body.is_in_group("protagonists") and switchfour == false:
