cea941ebbf44249d96eae6b95383e30ce6e176d9
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-03-23 18:22:33 +0000

    wip

--- src/levels/greklovick/GrekLevel5.gd
+++ src/levels/greklovick/GrekLevel5.gd
@@ -3,17 +3,24 @@ extends Node2D
 onready var animation = $AnimationPlayer
 onready var shieldgen1 = $ShieldGenerator1
 
+var switchespressed = 0
+
 
 #The leve could use a script of its own
 
 func _ready():
 	pass 
 
+#func _physics_process(delta):
+#	if switchespressed == 2:
+#		send_a_signal()
 
 func send_a_signal():
 	Signals.emit_signal("turn_off_zhark_shield")
 
+func switch_pressed():
+	switchespressed += 1
 
 func _on_ButtonPress_body_entered(body):
 	if body.is_in_group("protagonists"):
-		send_a_signal()
+		switch_pressed()
