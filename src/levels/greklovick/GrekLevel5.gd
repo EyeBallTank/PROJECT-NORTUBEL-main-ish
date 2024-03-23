27025f9d1ac695c1cc098b080f2b5cc72ad010ba
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-03-23 22:09:16 +0000

    so much stuff

--- src/levels/greklovick/GrekLevel5.gd
+++ src/levels/greklovick/GrekLevel5.gd
@@ -1,7 +1,20 @@
 extends Node2D
 
 onready var animation = $AnimationPlayer
+#yiegourattacksone
 onready var shieldgen1 = $ShieldGenerator1
+onready var shieldgen2 = $ShieldGenerator2
+onready var shieldgen3 = $ShieldGenerator3
+onready var shieldgen4 = $ShieldGenerator4
+onready var shieldgen5 = $ShieldGenerator5
+onready var shieldgen6 = $ShieldGenerator6
+
+var switchone = false
+var switchtwo = false
+var switchthree = false
+var switchfour = false
+var switchfive = false
+var switchsix = false
 
 var switchespressed = 0
 
@@ -21,7 +34,34 @@ func send_a_signal():
 func switch_pressed():
 	switchespressed += 1
 
+
+
 #func _on_ButtonPress_body_entered(body):
 #	if body.is_in_group("protagonists"):
 #		send_a_signal()
 #		animation.play("yiegourattacks")
+
+
+func _on_ButtonPress6_body_entered(body):
+	if body.is_in_group("protagonists"):
+		switchsix = true
+
+func _on_ButtonPress5_body_entered(body):
+	if body.is_in_group("protagonists"):
+		switchfive = true
+
+func _on_ButtonPress4_body_entered(body):
+	if body.is_in_group("protagonists"):
+		switchfour = true
+
+func _on_ButtonPress3_body_entered(body):
+	if body.is_in_group("protagonists"):
+		switchthree = true
+
+func _on_ButtonPress2_body_entered(body):
+	if body.is_in_group("protagonists"):
+		switchtwo = true
+
+func _on_ButtonPress1_body_entered(body):
+	if body.is_in_group("protagonists"):
+		switchone = true
