4545874052d1a2adeac9f4e2414f71510ab590b5
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-03-24 15:51:37 +0000

    so far

--- src/bosses/amalia/AmaliaHand.gd
+++ src/bosses/amalia/AmaliaHand.gd
@@ -36,11 +36,11 @@ func _physics_process(delta):
 		SLAM:
 			animation.play("slamming")
 			velocity = 0
-			timer.start(4)
+			timer.start(5)
 			if timer.time_left == 0:
 				state = FLOATING
 		DEAD:
-			pass
+			animation.play("dying")
 
 func see_to_punch():
 	if not raycast.is_colliding(): return false
@@ -50,3 +50,11 @@ func see_to_punch():
 
 func return_to_move():
 	state = FLOATING
+
+func die():
+	queue_free()
+
+
+func _on_Hurtbox_area_entered(area):
+	if area.name == "PlayerMelee":
+		state = DEAD
