a9fc697670fa018619e8da22c1a063eebe9f8871
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-03-24 15:52:33 +0000

    save a bit more

--- src/levels/nortubelpartfour/NortPartFourLevel3.gd
+++ src/levels/nortubelpartfour/NortPartFourLevel3.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 onready var animation = $AnimationPlayer 
 
+var phase = 1
+
 #Just in case
 func _ready():
 	animation.play("RESET")
+
+func send_a_signal():
+	Signals.emit_signal("collectible_picked")
