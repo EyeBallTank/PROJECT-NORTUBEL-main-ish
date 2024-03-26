2b6ad620b4a1b61f0e049a975cfb81b3f7e6a6e0
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-03-26 13:48:40 +0000

    stuff

--- src/play_actors/UulgaComp.gd
+++ src/play_actors/UulgaComp.gd
@@ -1,5 +1,25 @@
 extends KinematicBody2D
+class_name UulgaMain
+
+enum {
+	FOLLOWME,
+	STANDSTILL,
+	RUNAWAY,
+	ATTACKFROMFOLLOW,
+	ATTACKFROMRUN,
+	ATTACKFROMIDLE
+}
+
+var state = STANDSTILL
 
 
 func _ready():
 	pass 
+
+func return_to_move():
+	if state == ATTACKFROMFOLLOW:
+		FOLLOWME
+	elif state == ATTACKFROMRUN:
+		RUNAWAY
+	elif state == ATTACKFROMIDLE:
+		STANDSTILL
