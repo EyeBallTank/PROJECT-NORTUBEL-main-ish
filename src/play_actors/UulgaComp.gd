6c536bc6fa90f2eed90a9a0c8e0a584e2572322c
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-03-28 21:46:14 +0000

    save

--- src/play_actors/UulgaComp.gd
+++ src/play_actors/UulgaComp.gd
@@ -13,16 +13,16 @@ enum {
 var state = STANDSTILL
 
 var WALK_FORCE = 1600
-var WALK_MAX_SPEED = 700
+var WALK_MAX_SPEED = 400
 var STOP_FORCE = 450
-var JUMP_SPEED = 1500
+var JUMP_SPEED = 1800
 
 export var speed: = Vector2(300.0, 1000.0)
 export var gravity: = 1450.0
 onready var animatedsprite = $AnimatedSprite
 onready var enemycheck = $RayCast2D
 
-onready var uulgatateteller = $CanvasLayer/UulgaStateTeller
+onready var uulgastateteller = $CanvasLayer/UulgaStateTeller
 onready var audioplayer = $AudioStreamPlayer
 var was_on_floor = true
 
@@ -50,7 +50,7 @@ func _physics_process(delta):
 		FOLLOWME:
 			if see_to_attack():
 				state = ATTACKFROMFOLLOW
-			uulgatateteller.play("followstate")
+			uulgastateteller.play("followstate")
 
 			if Player.global_position.x < global_position.x - 10:
 				vel.x = -WALK_MAX_SPEED
@@ -61,7 +61,7 @@ func _physics_process(delta):
 			else:
 				vel.x = 0
 				direction.x = 0
-			vel.x = direction.x * 630
+			vel.x = direction.x * 400
 
 			if direction.x == 1 and is_on_floor():
 				animatedsprite.animation = "Running"
@@ -105,6 +105,114 @@ func _physics_process(delta):
 
 			if Input.is_action_just_pressed("basicstellarun"):
 				state = RUNAWAY
+			if Input.is_action_just_pressed("basicstellastop"):
+				state = STANDSTILL
+
+		STANDSTILL:
+			if see_to_attack():
+				state = ATTACKFROMIDLE
+			uulgastateteller.play("stopstate")
+			vel.x = 0
+			direction.x = 0
+			vel.x = direction.x * 0
+
+			if is_on_floor() and Input.is_action_just_pressed("stellajump"):
+				vel.y = -JUMP_SPEED
+				if vel.y < 0:
+					vel.y += 500
+
+			vel.y += gravity * delta
+			gravity = 1450.0
+			vel = move_and_slide_with_snap(vel, Vector2.DOWN, Vector2.UP)
+
+			if is_on_floor() and not was_on_floor:
+				audioplayer.play()
+			was_on_floor = is_on_floor()
+
+			if vel.y < 0 and not is_on_floor():
+				animatedsprite.animation = "Jumpgoesup"
+				if direction.x == 1:
+					animatedsprite.flip_h = false
+				elif direction.x == -1:
+					animatedsprite.flip_h = true
+			elif vel.y > 0 and not is_on_floor():
+				animatedsprite.animation = "Jumpgoesdown"
+				if direction.x == 1:
+					animatedsprite.flip_h = false
+				elif direction.x == -1:
+					animatedsprite.flip_h = true
+
+			if is_on_floor():
+				animatedsprite.animation = "Idle"
+
+			if Input.is_action_pressed("stellacommand") and Input.is_action_just_pressed("right"):
+				state = FOLLOWME
+			if Input.is_action_pressed("stellacommand") and Input.is_action_just_pressed("left"):
+				state = RUNAWAY
+
+			if Input.is_action_just_pressed("basicstellarun"):
+				state = RUNAWAY
+			if Input.is_action_just_pressed("basicstellafollow"):
+				state = FOLLOWME
+
+		RUNAWAY:
+			if see_to_attack():
+				state = ATTACKFROMRUN
+			uulgastateteller.play("runstate")
+
+			if Player.global_position.x < global_position.x - 10:
+				vel.x = WALK_MAX_SPEED
+				direction.x = 1
+			elif Player.global_position.x > global_position.x + 10:
+				vel.x = -WALK_MAX_SPEED
+				direction.x = -1
+			else:
+				vel.x = 0
+				direction.x = 0
+			vel.x = direction.x * 400
+
+			if direction.x == 1 and is_on_floor():
+				animatedsprite.animation = "Running"
+				animatedsprite.flip_h = false
+			elif direction.x == -1 and is_on_floor():
+				animatedsprite.animation = "Running"
+				animatedsprite.flip_h = true
+			else:
+				animatedsprite.animation = "Idle"
+
+			vel.y += gravity * delta
+			gravity = 1450.0
+			vel = move_and_slide_with_snap(vel, Vector2.DOWN, Vector2.UP)
+
+			if is_on_floor() and Input.is_action_just_pressed("stellajump"):
+				vel.y = -JUMP_SPEED
+				if vel.y < 0:
+					vel.y += 500
+
+			if vel.y < 0 and not is_on_floor():
+				animatedsprite.animation = "Jumpgoesup"
+				if direction.x == 1:
+					animatedsprite.flip_h = false
+				elif direction.x == -1:
+					animatedsprite.flip_h = true
+			elif vel.y > 0 and not is_on_floor():
+				animatedsprite.animation = "Jumpgoesdown"
+				if direction.x == 1:
+					animatedsprite.flip_h = false
+				elif direction.x == -1:
+					animatedsprite.flip_h = true
+
+			if is_on_floor() and not was_on_floor:
+				audioplayer.play()
+			was_on_floor = is_on_floor()
+
+			if Input.is_action_pressed("stellacommand") and Input.is_action_just_pressed("right"):
+				state = FOLLOWME
+			if Input.is_action_pressed("stellacommand") and Input.is_action_just_pressed("down"):
+				state = STANDSTILL
+
+			if Input.is_action_just_pressed("basicstellafollow"):
+				state = FOLLOWME
 			if Input.is_action_just_pressed("basicstellastop"):
 				state = STANDSTILL
 
