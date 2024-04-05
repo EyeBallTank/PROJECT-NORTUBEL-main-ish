344f0355c205e11829616d8dfed98fcd4ab3f2ac
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-04-05 22:10:40 +0100

    new npc

---
+++ src/npc_actors/grek npc/SamathaNPC.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+onready var dialogue = $CanvasLayer
+onready var maintext = $CanvasLayer/NinePatchRect/Dialogue
+onready var sprite = $Sprite
+export(String) var customtext = "SAMATHA_NPC_TALK"
+
+onready var nametext = $CanvasLayer/NinePatchRect2/Name
+export(String) var customname = "Samatha Strycker"
+
+func _ready():
+	maintext.text = str (customtext)
+	nametext.text = str (customname)
+	dialogue.visible = false
+	sprite.play("MAINSTATE")
+
+
+func _on_TalktoArea_body_entered(body):
+	if body.name == "Player":
+		dialogue.visible = true
+
+func _on_TalktoArea_body_exited(body):
+	if body.name == "Player":
+		dialogue.visible = false
