3cbceb5b4f46050bc01dd27c831a9925cc52296f
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-05-12 21:57:27 +0100

    toy sound

--- src/level_hazards/brinkaedea/BrinkHammer.gd
+++ src/level_hazards/brinkaedea/BrinkHammer.gd
@@ -2,7 +2,12 @@ extends Node2D
 
 onready var animationplayer = $AnimationPlayer
 export var animation = "RESET"
-
+onready var sound = $AudioStreamPlayer
 
 func _ready():
 	animationplayer.play(animation)
+
+
+func _on_SoundArea_body_entered(body):
+	if body.is_in_group("protagonists"):
+		sound.play()
