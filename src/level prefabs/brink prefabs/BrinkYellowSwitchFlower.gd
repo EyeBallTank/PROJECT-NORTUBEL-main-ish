80e130f2dcef035304f12c232c04531e35df2ab9
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-05-15 21:21:03 +0100

    so far

--- src/level prefabs/brink prefabs/BrinkYellowSwitchFlower.gd
+++ src/level prefabs/brink prefabs/BrinkYellowSwitchFlower.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
 onready var animationplayer = $AnimationPlayer
+onready var sound = $AudioStreamPlayer
 
 func _ready():
 	animationplayer.play("RESET")
-	Signals.connect("yellow_switch_pressed", self, "_on_YellowSwitch_pressed")
 
-func _on_YellowSwitch_pressed():
-	print("YELPLAT IS ACTIVE")
-#	Signals.emit_signal("yellow_plat_verti_active")
-	animationplayer.play("growing")
-	yield(get_tree().create_timer(9), "timeout")
-	animationplayer.play_backwards("growing")
+
+
+func _on_Flowerswitch_body_entered(body):
+	if body.is_in_group("protagonists"):
+		sound.play()
+		animationplayer.play("growing")
+		yield(get_tree().create_timer(16), "timeout")
+		animationplayer.play_backwards("growing")
+		yield(animationplayer, "animation_finished")
+		animationplayer.play("RESET")
