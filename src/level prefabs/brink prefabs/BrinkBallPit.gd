5f22636393a6fb5d87724e915c0987f600e3bffb
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-05-25 22:11:51 +0100

    it is what it is

--- src/level prefabs/brink prefabs/BrinkBallPit.gd
+++ src/level prefabs/brink prefabs/BrinkBallPit.gd
@@ -1,6 +1,18 @@
 extends StaticBody2D
 
+onready var animation = $AnimationPlayer
 
 
 func _ready():
 	pass 
+
+#maybe the animation could be tweaked in the future
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("protagonists"):
+		animation.play("BallsBounce")
+
+
+func _on_Area2D_body_exited(body):
+	if body.is_in_group("protagonists"):
+		animation.play("BallsBounce")
