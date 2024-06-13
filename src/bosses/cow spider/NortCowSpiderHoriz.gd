f881b21573d641e50fd80124c1aad63c5ee30be5
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-06-13 14:17:33 +0100

    spider so far

--- src/bosses/cow spider/NortCowSpiderHoriz.gd
+++ src/bosses/cow spider/NortCowSpiderHoriz.gd
@@ -23,7 +23,7 @@ func _physics_process(delta):
 			if found_wall:
 				direction *= -1
 				sprite.flip_h = direction.x > 0
-			velocity = direction * 290
+			velocity = direction * 390
 			move_and_slide(velocity, Vector2.UP)
 		DEAD:
 			animation.play("dying")
@@ -31,3 +31,9 @@ func _physics_process(delta):
 
 func die():
 	queue_free()
+
+func _on_SpiderHurtbox_area_entered(area):
+	if area.name == "PlayerMelee":
+		state = DEAD
+	if area.name == "EnemyCrusher":
+		state = DEAD
