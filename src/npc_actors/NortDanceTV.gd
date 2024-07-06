5ad315928d52176574a437003719b9145916b973
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-07-06 18:55:51 +0100

    whatever

---
+++ src/npc_actors/NortDanceTV.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+
+onready var dialoguebox = $CanvasLayer
+onready var sprite = $CanvasLayer/DanceEasterEgg
+onready var basicmessage = $Basicmessage
+
+
+var test = false
+
+func _ready():
+	dialoguebox.visible = false
+	basicmessage.visible = false
+
+func _physics_process(delta):
+	if test == true:
+		if Input.is_action_just_pressed("standstill") and dialoguebox.visible == false:
+			dialoguebox.visible = true
+	elif test == false:
+		dialoguebox.visible = false
+	if Input.is_action_just_pressed("runaway"):
+		test = false
+
+
+func _on_TalktoArea_body_entered(body):
+	if body.name == "Player":
+		basicmessage.visible = true
+		test = true
+
+func _on_TalktoArea_body_exited(body):
+	if body.name == "Player":
+		basicmessage.visible = false
+		test = false
+
+
