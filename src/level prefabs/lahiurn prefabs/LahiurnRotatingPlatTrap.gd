ff48c66d6590b77b16b8a16f4372cf7ca912c075
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-07-07 14:08:13 +0100

    lahiurn trap

---
+++ src/level prefabs/lahiurn prefabs/LahiurnRotatingPlatTrap.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+onready var switchsprite = $BlueSwitch/AnimatedSprite
+onready var animation = $AnimationPlayer
+
+func _ready():
+	switchsprite.play("switchon")
+	animation.play("RESET")
+
+
+func _on_BlueSwitch_body_entered(body):
+	if body.is_in_group("protagonists"):
+		animation.play("active")
