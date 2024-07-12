8ec1848ff0344c157c01fb87219983965612cf85
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-07-12 22:31:56 +0100

    a lot can go wrong

---
+++ src/level prefabs/lahiurn prefabs/LahiurnMountedTurretGun.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+var honk = false
+onready var animation = $AnimationPlayer
+onready var gunhole = $Position2D
+
+const Bullet = preload("res://src/enemies/krimbstone/FriendlyBullet.tscn")
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	if honk == true:
+		if Input.is_action_just_pressed("standstill"):
+			pass
+
+func attack():
+	var projectile = Bullet.instance()
+	projectile.global_position = gunhole.global_position
+	projectile.velocity.x = projectile.speed * 10
+	get_tree().get_root().add_child(projectile)
+
+func _on_UseIt_body_entered(body):
+	if body.name == "Player":
+		honk = true
+
+func _on_UseIt_body_exited(body):
+	if body.name == "Player":
+		honk = false
