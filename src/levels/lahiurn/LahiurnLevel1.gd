f61f60fecb9ebf09ffa3eb0ca1c71883c3c8920b
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-07-14 21:28:58 +0100

    anyway

--- src/levels/lahiurn/LahiurnLevel1.gd
+++ src/levels/lahiurn/LahiurnLevel1.gd
@@ -1,16 +1,9 @@
 extends Node2D
 
+onready var background = $ParallaxBackground2
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _process(delta):
+	background.scroll_base_offset -= Vector2(-30, 0) * delta
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	pass
