c63d249bd42adfafccf724a9b248870cce1eb743
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-07-26 16:00:27 +0100

    making it work somewhat

--- src/level prefabs/lahiurn prefabs/LahiurnWallDestroyableWood.gd
+++ src/level prefabs/lahiurn prefabs/LahiurnWallDestroyableWood.gd
@@ -2,13 +2,19 @@ extends StaticBody2D
 
 onready var animation = $AnimationPlayer
 
+var health = 5
+
 func _ready():
 	animation.play("RESET")
 
 func die():
 	queue_free()
 
+#func _physics_process(delta):
+#	if health == 0:
+#		animation.play("wall_is_gone")
 
 func _on_BulletCatcher_body_entered(body):
 	if body.is_in_group("goodbullet"):
+#		health =- 1
 		animation.play("wall_is_gone")
