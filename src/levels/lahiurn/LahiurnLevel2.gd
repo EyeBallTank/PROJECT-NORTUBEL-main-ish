467451796efdb3976c96848cc45557e01aa53c3d
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-08-01 14:45:51 +0100

    so far

--- src/levels/lahiurn/LahiurnLevel2.gd
+++ src/levels/lahiurn/LahiurnLevel2.gd
@@ -1,14 +1,17 @@
 extends Node2D
 
 onready var animation = $AnimationPlayer
+onready var otheranimation = $AnimationPlayer2
 
 
 func _ready():
 	animation.play("RESET")
+	otheranimation.play("WaterDown")
 	Signals.connect("blue_switch_pressed", self, "_sub_alarm_is_on")
 
 func _sub_alarm_is_on():
 	animation.play("alarmon")
+	otheranimation.play("WaterUp")
 
 #IDEA: IF BLUE SWITCH PRESSED, WATER RISES UP (LIKE IN MASSAGLEVEL2) AND A HORIZONTAL DOOR
 #OPENS TO REVEAL AN UPPER ROOM NOT AFFECTED BY WATER
