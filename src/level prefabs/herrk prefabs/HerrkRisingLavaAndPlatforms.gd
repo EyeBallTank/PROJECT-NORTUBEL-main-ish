ec12fd7703066ad428418f3fce297e028a42cd3a
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-08-23 20:38:51 +0100

    wip

---
+++ src/level prefabs/herrk prefabs/HerrkRisingLavaAndPlatforms.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+onready var animationplayer = $AnimationPlayer
+onready var sound = $AudioStreamPlayer
+
+func _ready():
+	animationplayer.play("RESET")
+
+
+
+
+func _on_Flowerswitch_body_entered(body):
+	if body.is_in_group("protagonists"):
+		sound.play()
+		animationplayer.play("growing")
+		yield(get_tree().create_timer(21), "timeout")
+		animationplayer.play_backwards("growing")
+		yield(animationplayer, "animation_finished")
+		animationplayer.play("RESET")
