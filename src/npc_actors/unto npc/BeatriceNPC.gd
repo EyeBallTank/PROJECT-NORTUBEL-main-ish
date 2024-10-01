7c5f2c016e1294e40cdab67d27f3a0f8c262cf86
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-10-01 22:13:21 +0100

    way more

--- src/npc_actors/unto npc/BeatriceNPC.gd
+++ src/npc_actors/unto npc/BeatriceNPC.gd
@@ -1,16 +1,46 @@
 extends Node2D
 
+var honktwo = false
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var dialogue = $CanvasLayer
+onready var maintext = $CanvasLayer/NinePatchRect/Dialouge
+onready var animationplayer = $AnimationPlayer
 
+export(String) var customtext = "BEATRICE_NPC_TALK"
+export(String) var customtext2 = "BEATRICE_NPC_HUG"
+export(String) var customtext3 = "BEATRICE_NPC_HURT"
+export(String) var customtext4 = "BEATRICE_NPC_HONK"
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	animationplayer.play("RESET")
+	maintext.text = str (customtext)
+	dialogue.visible = false
+
+func _physics_process(delta):
+	if honktwo == true:
+		if Input.is_action_just_pressed("followme"):
+			maintext.text = str (customtext2)
+		if Input.is_action_just_pressed("attack"):
+			maintext.text = str (customtext3)
+		if Input.is_action_pressed("kickball") and Input.is_action_just_pressed("standstill"):
+			maintext.text = str (customtext4)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_TalkToArea_body_entered(body):
+	if body.name == "Player":
+		dialogue.visible = true
+		honktwo = true
+
+
+func _on_TalkToArea_body_exited(body):
+	if body.name == "Player":
+		dialogue.visible = false
+		honktwo = false
+		maintext.text = str (customtext)
+
+
+
+
+func _on_FreeMe_area_entered(area):
+	if area.name == "PlayerMelee":
+		animationplayer.play("free")
