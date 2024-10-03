5afa1c39aba9fa5efe5e9ed7ae843bcd61fde98b
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-10-03 21:47:46 +0100

    giant zombie hand done so far

--- src/level_hazards/untotern/UntoGiantZombieHand.gd
+++ src/level_hazards/untotern/UntoGiantZombieHand.gd
@@ -1,6 +1,23 @@
 extends Node2D
 
-
+onready var animationplayer = $AnimationPlayer
 
 func _ready():
-	pass
+	animationplayer.play("RESET")
+
+func die():
+	queue_free()
+
+
+
+
+func _on_EnterHand_body_entered(body):
+	if body.is_in_group("protagonists"):
+		animationplayer.play("caught")
+
+
+func _on_HURTBOX_area_entered(area):
+	if area.name == "PlayerMelee":
+		animationplayer.play("dead")
+	if area.name == "EnemyCrusher":
+		animationplayer.play("dead")
