2d5242187b00b6d39083a316438fb4c278eead2d
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-12-07 10:26:59 +0000

    another song

--- src/levels/lahiurn/LahiurnLevel2.gd
+++ src/levels/lahiurn/LahiurnLevel2.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 onready var animation = $AnimationPlayer
 onready var otheranimation = $AnimationPlayer2
-
+onready var musicplayer = $MusicPlayer
 
 func _ready():
 	animation.play("RESET")
