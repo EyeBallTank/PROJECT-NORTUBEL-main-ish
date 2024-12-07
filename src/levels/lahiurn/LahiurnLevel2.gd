98c45ce04f1ea3fd001af05e9963ea6bf88bc464
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-12-07 21:24:40 +0000

    oh well

--- src/levels/lahiurn/LahiurnLevel2.gd
+++ src/levels/lahiurn/LahiurnLevel2.gd
@@ -12,6 +12,7 @@ func _ready():
 func _sub_alarm_is_on():
 	animation.play("alarmon")
 	otheranimation.play("WaterUp")
+	musicplayer.play("WaterMusic")
 
 #IDEA: IF BLUE SWITCH PRESSED, WATER RISES UP (LIKE IN MASSAGLEVEL2) AND A HORIZONTAL DOOR
 #OPENS TO REVEAL AN UPPER ROOM NOT AFFECTED BY WATER
