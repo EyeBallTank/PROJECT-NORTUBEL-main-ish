61e517a0c8556bb264db3613d768cc0956e60117
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-12-25 18:14:44 +0000

    HUGE

--- src/levels/nortubelpartfive/NortPartFiveLevel3.gd
+++ src/levels/nortubelpartfive/NortPartFiveLevel3.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
 onready var animation = $AnimationPlayer
-
-
+onready var musicplayer = $MusicPlayer
+onready var whenyouwin = $WhenYouWin
 
 func _ready():
-	pass
+	musicplayer.play("RESET")
+	whenyouwin.play("RESET")
+
+
+func _on_WhenSpidersAreGone_body_entered(body):
+	if body.is_in_group("protagonists"):
+		musicplayer.play("SpidersAreGone")
+		whenyouwin.play("GoodBye")
