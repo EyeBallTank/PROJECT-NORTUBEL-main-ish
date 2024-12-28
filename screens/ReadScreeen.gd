6902326aeaba17d129322dd7751bf0407eb2479a
EyeBall Tank <93493280+EyeBallTank@users.noreply.github.com>
2024-12-28 11:27:57 +0000

    so far

--- screens/ReadScreeen.gd
+++ screens/ReadScreeen.gd
@@ -3,5 +3,10 @@ extends Control
 func _ready():
 	pass
 
-func _on_Button_pressed():
+func _on_TextureButton_pressed():
 	SceneManager.change_scene("level_transition_effect", "res://screens/MainMenu.tscn")
+
+
+func _on_TextureButton2_pressed():
+	SceneManager.change_scene("level_transition_effect", "res://screens/cutscenes/cutscene 34 nortubel credits/cutscene 34 nortubel credits.tscn")
+
--- screens/cutscenes/eyeballtank presents/StartUp.gd
+++ screens/cutscenes/eyeballtank presents/StartUp.gd
@@ -4,6 +4,7 @@ onready var animationplayer = $AnimationPlayer
 
 func _ready():
 	animationplayer.play("TITLE SEQUENCE")
+	VisualServer.set_default_clear_color(Color.darkolivegreen)
 
 func changescene():
 	SceneManager.change_scene("level_transition_effect", "res://screens/MainMenu.tscn")
